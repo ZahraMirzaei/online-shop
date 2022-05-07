0d9de05631507d69d463c15a27576a821ee8fc97
ZahraMirzaei.JS <z.mirzaei2019@gmail.com>
2022-05-07 12:55:39 +0430

    perf: :zap: Add purgecss to postcss.config
    Change postcss.config.js file to use purgecss tools to decrease css file size in production mode

--- postcss.config.js
+++ postcss.config.js
@@ -1,6 +1,28 @@
 module.exports = {
-  plugins: {
-    tailwindcss: {},
-    autoprefixer: {},
-  },
-}
+  plugins: [
+    require("tailwindcss"),
+    require("autoprefixer"),
+    process.env.NODE_ENV === "production" &&
+      require("@fullhuman/postcss-purgecss")({
+        content: [
+          "./pages/**/*.{js,ts,jsx,tsx}",
+          "./components/**/*.{js,ts,jsx,tsx}",
+        ],
+        defaultExtractor: (content) => content.match(/[A-Za-Z0-9-_:/]+/g) || [],
+      }),
+  ],
+};
+
+// const purgecss = require("@fullhuman/postcss-purgecss");
+// module.exports = {
+//   plugins: {
+//     tailwindcss: {},
+//     autoprefixer: {},
+//     purgecss: purgecss({
+//       content: [
+//         "./pages/**/*.{js,ts,jsx,tsx}",
+//         "./components/**/*.{js,ts,jsx,tsx}",
+//       ],
+//     }),
+//   },
+// };
