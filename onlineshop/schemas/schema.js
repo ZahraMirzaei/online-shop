b47bbe7a47c46b63b60d7930f2a65ffc3b04e9fd
ZahraMirzaei.JS <z.mirzaei2019@gmail.com>
2022-05-14 07:41:00 +0430

    feat: :bento: add product and banner to sanity schema

--- onlineshop/schemas/schema.js
+++ onlineshop/schemas/schema.js
@@ -1,16 +1,21 @@
 // First, we must import the schema creator
-import createSchema from 'part:@sanity/base/schema-creator'
+import createSchema from "part:@sanity/base/schema-creator";
 
 // Then import schema types from any plugins that might expose them
-import schemaTypes from 'all:part:@sanity/base/schema-type'
+import schemaTypes from "all:part:@sanity/base/schema-type";
+
+import product from "./product";
+import banner from "./banner";
 
 // Then we give our schema to the builder and provide the result to Sanity
 export default createSchema({
   // We name our schema
-  name: 'default',
+  name: "default",
   // Then proceed to concatenate our document type
   // to the ones provided by any plugins that are installed
   types: schemaTypes.concat([
     /* Your types here! */
+    product,
+    banner,
   ]),
-})
+});
