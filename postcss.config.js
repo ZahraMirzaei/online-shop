d0aac7aac4beb9cc6cce07dbc18f824f72b619e9
ZahraMirzaei.JS <z.mirzaei2019@gmail.com>
2022-05-14 08:59:11 +0430

    feat: :wrench: refactore postcss.config.js file to support purgecss and cssnano plugins

--- postcss.config.js
+++ postcss.config.js
@@ -1,28 +1,21 @@
 module.exports = {
   plugins: [
-    require("tailwindcss"),
-    require("autoprefixer"),
-    process.env.NODE_ENV === "production" &&
-      require("@fullhuman/postcss-purgecss")({
-        content: [
-          "./pages/**/*.{js,ts,jsx,tsx}",
-          "./components/**/*.{js,ts,jsx,tsx}",
-        ],
-        defaultExtractor: (content) => content.match(/[A-Za-Z0-9-_:/]+/g) || [],
-      }),
+    "tailwindcss",
+    "autoprefixer",
+    process.env.NODE_ENV === "production"
+      ? [
+          "@fullhuman/postcss-purgecss",
+          {
+            content: [
+              "./pages/**/*.{js,jsx,ts,tsx}",
+              "./components/**/*.{js,jsx,ts,tsx}",
+            ],
+            defaultExtractor: (content) =>
+              content.match(/[\w-/:]+(?<!:)/g) || [],
+          },
+        ]
+      : undefined,
+    "postcss-preset-env",
+    ...(process.env.NODE_ENV === "production" ? { cssnano: {} } : {}),
   ],
 };
-
-// const purgecss = require("@fullhuman/postcss-purgecss");
-// module.exports = {
-//   plugins: {
-//     tailwindcss: {},
-//     autoprefixer: {},
-//     purgecss: purgecss({
-//       content: [
-//         "./pages/**/*.{js,ts,jsx,tsx}",
-//         "./components/**/*.{js,ts,jsx,tsx}",
-//       ],
-//     }),
-//   },
-// };
