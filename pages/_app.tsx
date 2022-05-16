4c4c6d99aa42f95aa9170aff72240e4d0714a85d
ZahraMirzaei.JS <z.mirzaei2019@gmail.com>
2022-05-16 20:25:36 +0430

    style: :lipstick: import slick css files into _app
    slick package is used for slider section

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,8 +1,11 @@
-import '../styles/globals.css'
-import type { AppProps } from 'next/app'
+import "slick-carousel/slick/slick.css";
+import "slick-carousel/slick/slick-theme.css";
+
+import "../styles/globals.css";
+import type { AppProps } from "next/app";
 
 function MyApp({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  return <Component {...pageProps} />;
 }
 
-export default MyApp
+export default MyApp;
