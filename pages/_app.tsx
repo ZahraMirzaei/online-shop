e9e08564b7fe6197a84dcc88f872c7bf86cfdbce
ZahraMirzaei.JS <z.mirzaei2019@gmail.com>
2022-05-17 19:10:33 +0430

    feat: :sparkles: set direction base on languge

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,11 +1,18 @@
+import type { AppProps } from "next/app";
+import { useRouter } from "next/router";
+
 import "slick-carousel/slick/slick.css";
 import "slick-carousel/slick/slick-theme.css";
 
 import "../styles/globals.css";
-import type { AppProps } from "next/app";
 
 function MyApp({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />;
+  const { locale } = useRouter();
+  return (
+    <div dir={locale === "fa" ? "rtl" : "ltr"}>
+      <Component {...pageProps} />
+    </div>
+  );
 }
 
 export default MyApp;
--- pages/_document.tsx
+++ pages/_document.tsx
@@ -1,8 +1,7 @@
 import { Html, Head, Main, NextScript } from "next/document";
-
 export default function Document() {
   return (
-    <Html lang="fa" dir="rtl">
+    <Html>
       <Head />
       <body>
         <Main />
