df62c4ca0eb8e21c984516ecb7aff73fd246326d
ZahraMirzaei.JS <z.mirzaei2019@gmail.com>
2022-05-17 19:09:05 +0430

    style: :wrench: confige color palette to tailwind.config.js

--- tailwind.config.js
+++ tailwind.config.js
@@ -4,7 +4,27 @@ module.exports = {
     "./components/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        palette: {
+          primary: "var(--color-primary)",
+          secondary: "var(--color-secondary)",
+        },
+      },
+      textColor: {
+        palette: {
+          base: "var(--color-text-base)",
+          mute: "var(--color-text-muted)",
+          side: "var(--color-text-side)",
+        },
+      },
+      backgroundColor: {
+        palette: {
+          fill: "var(--color-bg)",
+          card: "var(--color-bg-side)",
+        },
+      },
+    },
   },
   plugins: [],
 };
