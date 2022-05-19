1c653ff7111ab27a5f9179eeeda6f837f4d8f437
ZahraMirzaei.JS <z.mirzaei2019@gmail.com>
2022-05-20 00:24:38 +0430

    feat: :lipstick: add poppins font links to _document.tsx

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -2,7 +2,18 @@ import { Html, Head, Main, NextScript } from "next/document";
 export default function Document() {
   return (
     <Html>
-      <Head />
+      <Head>
+        <link rel="preconnect" href="https://fonts.googleapis.com" />
+        <link
+          rel="preconnect"
+          href="https://fonts.gstatic.com"
+          crossOrigin="true"
+        />
+        <link
+          href="https://fonts.googleapis.com/css2?family=Poppins:wght@100;300;400;500&display=swap"
+          rel="stylesheet"
+        />
+      </Head>
       <body>
         <Main />
         <NextScript />
