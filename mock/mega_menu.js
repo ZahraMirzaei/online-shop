d9142a250bb1f9210858b7d52711fff56f95f1f7
ZahraMirzaei.JS <z.mirzaei2019@gmail.com>
2022-05-21 19:06:46 +0430

    feat: :clown_face: write mega menu mock

---
+++ mock/mega_menu.js
@@ -0,0 +1,79 @@
+const megaMenu = [
+  {
+    category: "Digital",
+    icon: "BsLaptop",
+    productsGroup: [
+      {
+        title: "Laptop",
+        subtitles: [
+          "Asus",
+          "Apple",
+          "Dell",
+          "Lenovo",
+          "Samsung",
+          "HP",
+          "HUAWEI",
+          "Acer",
+          "MSI",
+        ],
+      },
+      {
+        title: "Mobile",
+        subtitles: [
+          "Samsung",
+          "Apple",
+          "Nokia",
+          "Xiaomi",
+          "Motorola",
+          "LG",
+          "Sony",
+        ],
+      },
+      {
+        title: "Computer",
+        subtitles: ["Monitor", "Mouse", "Keyboard", "Hard"],
+      },
+      {
+        title: "Other",
+        subtitles: ["Tablet", "Flash", "PowerBank", "Speaker", "Headphone"],
+      },
+    ],
+  },
+  {
+    category: "fashion",
+    icon: "IoShirtOutline",
+    productsGroup: [
+      {
+        title: "women",
+        subtitles: [
+          "dress",
+          "Skirt ",
+          "jeans",
+          "pants",
+          "T-shirt",
+          "scarf",
+          "HUAWEI",
+          "Acer",
+          "MSI",
+        ],
+      },
+      {
+        title: "men",
+        subtitles: ["shirt", "pants", "tie", "T-shirt", "jeans", "LG", "Sony"],
+      },
+      {
+        title: "child",
+        subtitles: ["Monitor", "Mouse", "keyboard", "Hard", "Ram"],
+      },
+      {
+        title: "other",
+        subtitles: ["watch", "wallet", "hat", "belt", "shoes"],
+      },
+    ],
+  },
+  { category: "toys", icon: "MdOutlineToys" },
+  { category: "Cosmetic", icon: "RiHeartPulseLine" },
+  { category: "Home", icon: "AiOutlineHome" },
+  { category: "Sport", icon: "MdSportsTennis" },
+  { category: "Stationery", icon: "BsBook" },
+];
