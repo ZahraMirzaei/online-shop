e0e7eeb469db81f4ff9896b2919794978570d058
ZahraMirzaei.JS <z.mirzaei2019@gmail.com>
2022-05-21 20:20:35 +0430

    feat: :sparkles: add theme component

---
+++ components/header/Theme.tsx
@@ -0,0 +1,36 @@
+import React, { useState, useEffect } from "react";
+import { useTheme } from "next-themes";
+import { BsFillMoonStarsFill } from "react-icons/bs";
+import { BsFillBrightnessHighFill } from "react-icons/bs";
+
+const Theme = () => {
+  const [mounted, setMounted] = useState(false);
+  useEffect(() => {
+    setMounted(true);
+  }, []);
+  const { systemTheme, theme, setTheme } = useTheme();
+  const currentTheme = theme === "system" ? systemTheme : theme;
+  const renderThemeChanger = () => {
+    if (!mounted) return null;
+    if (currentTheme === "dark") {
+      return (
+        <div
+          className="w-7 h-7"
+          role="button"
+          onClick={() => setTheme("light")}
+        >
+          <BsFillBrightnessHighFill style={{ fontSize: "1.5rem" }} />
+        </div>
+      );
+    } else {
+      return (
+        <div className="w-7 h-7" role="button" onClick={() => setTheme("dark")}>
+          <BsFillMoonStarsFill style={{ fontSize: "1.5rem" }} />
+        </div>
+      );
+    }
+  };
+  return <>{renderThemeChanger()}</>;
+};
+
+export default Theme;
