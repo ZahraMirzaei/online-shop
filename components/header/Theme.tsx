f682cd59d274aaf1192d8c27487ddad672819482
ZahraMirzaei.JS <z.mirzaei2019@gmail.com>
2022-05-22 16:11:45 +0430

    style: :lipstick: change theme styles
    change space styling

--- components/header/Theme.tsx
+++ components/header/Theme.tsx
@@ -5,31 +5,31 @@ import { BsFillBrightnessHighFill } from "react-icons/bs";
 
 const Theme = () => {
   const [mounted, setMounted] = useState(false);
+
   useEffect(() => {
     setMounted(true);
   }, []);
+
   const { systemTheme, theme, setTheme } = useTheme();
   const currentTheme = theme === "system" ? systemTheme : theme;
+
   const renderThemeChanger = () => {
     if (!mounted) return null;
     if (currentTheme === "dark") {
       return (
-        <div
-          className="w-7 h-7"
-          role="button"
-          onClick={() => setTheme("light")}
-        >
+        <div role="button" onClick={() => setTheme("light")}>
           <BsFillBrightnessHighFill style={{ fontSize: "1.5rem" }} />
         </div>
       );
     } else {
       return (
-        <div className="w-7 h-7" role="button" onClick={() => setTheme("dark")}>
-          <BsFillMoonStarsFill style={{ fontSize: "1.5rem" }} />
+        <div role="button" onClick={() => setTheme("dark")}>
+          <BsFillMoonStarsFill style={{ fontSize: "1.3rem" }} />
         </div>
       );
     }
   };
+
   return <>{renderThemeChanger()}</>;
 };
 
