55155ab6ab1cf37bddbc5e79fdf6589617c21fc5
ZahraMirzaei.JS <z.mirzaei2019@gmail.com>
2022-05-23 10:45:50 +0430

    feat: :sparkles: add Transition and SideNav components to Header and update SideNave with forwardRef and displayName
    when I use Transition component, an error happened. That was because of bug in Transition component so I use forwardRef  and then displayName.

--- components/header/SideNav.tsx
+++ components/header/SideNav.tsx
@@ -1,16 +1,35 @@
-import React from "react";
+import React, { forwardRef } from "react";
 import { useRouter } from "next/router";
-const SideNav = () => {
+
+interface Props {
+  state?: string;
+  children?: React.ReactNode;
+  ref: React.HTMLProps<HTMLDivElement>;
+}
+
+const SideNav = forwardRef<HTMLDivElement, Props>(({ state }, ref) => {
   const { locale } = useRouter();
   return (
     <div
-      className={`max-w-[380px] w-[80%] h-screen fixed top-0 ${
-        locale == "en" ? "left-0" : "right-0"
-      } z-[100] bg-palette-card origin-left overflow-hidden ${
-        locale == "en" ? "translate-x-[-100%]" : "translate-x-[100%]"
-      }`}
-    ></div>
+      ref={ref}
+      className={`max-w-[380px] w-[80%] h-screen fixed top-0 shadow-md
+      ${locale == "en" ? "left-0" : "right-0"} 
+      z-[100] bg-palette-card origin-left overflow-hidden 
+      ${locale == "en" ? "translate-x-[-100%]" : "translate-x-[100%]"}
+      ${
+        state === "entering"
+          ? "ltr:animate-sidenavLTREntering rtl:animate-sidenavRTLEntering"
+          : state === "entered"
+          ? "translate-x-0"
+          : "ltr:animate-sidenavLTRExit rtl:animate-sidenavRTLExit"
+      }
+      `}
+    >
+      test
+    </div>
   );
-};
+});
+
+SideNav.displayName = "SideNav";
 
 export default SideNav;
--- components/header/index.tsx
+++ components/header/index.tsx
@@ -1,21 +1,54 @@
-import React from "react";
+import React, { useState, useRef } from "react";
 import Link from "next/link";
 import Image from "next/image";
 import { useRouter } from "next/router";
+import { Transition } from "react-transition-group";
 import en from "../../locales/pages/home/en";
 import fa from "../../locales/pages/home/fa";
 import SearchBar from "./SearchBar";
 import { GoGrabber } from "react-icons/go";
 import Theme from "./Theme";
 import Language from "./Language";
+import SideNav from "./SideNav";
 const Header = () => {
+  const [navOpen, setNavOpen] = useState(false);
+  const nodeRef = useRef<HTMLDivElement>(null);
   const { locale } = useRouter();
   const t = locale === "en" ? en : fa;
+
+  const openNav = () => {
+    setNavOpen(true);
+  };
+  const closeNav = () => {
+    setNavOpen(false);
+  };
+
   return (
     <div className=" pt-4">
       <div>
         <div className="flex justify-between">
-          <GoGrabber style={{ fontSize: "2.5rem" }} />
+          <div onClick={openNav}>
+            <GoGrabber style={{ fontSize: "2.5rem" }} />
+          </div>
+          <Transition
+            nodeRef={nodeRef}
+            in={navOpen}
+            timeout={300}
+            mountOnEnter
+            unmountOnExit
+          >
+            {(state) => {
+              return (
+                <>
+                  <SideNav ref={nodeRef} state={state} />
+                  <div className="fixed inset-0 z-50 bg-palette-dark/[0.6]"></div>
+                  <div
+                    className={`absolute top-0 ltr:left-0 rtl:right-0`}
+                  ></div>
+                </>
+              );
+            }}
+          </Transition>
           <Link href="/">
             <h1>
               <Image
