396cee9c45cb55c3b220cb95a8b6807a5c0a9bf8
ZahraMirzaei.JS <z.mirzaei2019@gmail.com>
2022-05-24 20:39:29 +0430

    feat: :sparkles: add state to control the visibility of language box

--- components/header/Language.tsx
+++ components/header/Language.tsx
@@ -9,6 +9,8 @@ const Language = () => {
   const t = locale === "en" ? en : fa;
   const [lang, setLang] = useState(locale);
 
+  const [openLang, setOpenLang] = useState(false);
+
   useEffect(() => {
     document.documentElement.dir = locale === "en" ? "ltr" : "rtl";
   }, [locale]);
@@ -19,42 +21,57 @@ const Language = () => {
 
   return (
     <div className=" relative">
-      <MdLanguage style={{ fontSize: "1.4rem" }} />
-      <div
-        className={`absolute top-8 ltr:right-0 rtl:left-0 bg-palette-card py-3 px-6 shadow-md rounded-md`}
-      >
-        <Link href="/" locale="fa">
-          <a className="whitespace-nowrap flex justify-between items-center">
-            <input
-              type="radio"
-              id="fa"
-              name="language"
-              value={lang}
-              checked={locale === "fa" ? true : false}
-              onChange={onChangeHandler}
-            />
-            <label htmlFor="fa" className="font-farsi mx-3 grow">
-              {t.farsi}
-            </label>
-          </a>
-        </Link>
+      <MdLanguage
+        style={{ fontSize: "1.4rem" }}
+        onClick={() => setOpenLang((prevState) => !prevState)}
+      />
+      {openLang ? (
+        <>
+          <div
+            className="fixed inset-0 -z-1"
+            onClick={() => setOpenLang(false)}
+          ></div>
+          <div
+            className={`absolute top-8 ltr:right-0 rtl:left-0 bg-palette-card py-3 px-6 shadow-md rounded-md`}
+          >
+            <Link href="/" locale="fa">
+              <a className="whitespace-nowrap flex justify-between items-center">
+                <div onClick={() => setOpenLang(false)}>
+                  <input
+                    type="radio"
+                    id="fa"
+                    name="language"
+                    value={lang}
+                    checked={locale === "fa" ? true : false}
+                    onChange={onChangeHandler}
+                  />
+                  <label htmlFor="fa" className="font-farsi mx-3 grow">
+                    {t.farsi}
+                  </label>
+                </div>
+              </a>
+            </Link>
 
-        <Link href="/" locale="en">
-          <a className="whitespace-nowrap flex justify-start items-center mt-3">
-            <input
-              type="radio"
-              id="en"
-              name="language"
-              value={lang}
-              checked={locale === "en" ? true : false}
-              onChange={onChangeHandler}
-            />
-            <label htmlFor="en" className=" mx-3 grow">
-              {t.english}
-            </label>
-          </a>
-        </Link>
-      </div>
+            <Link href="/" locale="en">
+              <a className="whitespace-nowrap flex justify-start items-center mt-3">
+                <div onClick={() => setOpenLang(false)}>
+                  <input
+                    type="radio"
+                    id="en"
+                    name="language"
+                    value={lang}
+                    checked={locale === "en" ? true : false}
+                    onChange={onChangeHandler}
+                  />
+                  <label htmlFor="en" className=" mx-3 grow">
+                    {t.english}
+                  </label>
+                </div>
+              </a>
+            </Link>
+          </div>
+        </>
+      ) : null}
     </div>
   );
 };
