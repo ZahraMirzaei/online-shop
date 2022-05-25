3d4e6c53e24e47d21c8cc7964b6569b9f9e4c1a1
ZahraMirzaei.JS <z.mirzaei2019@gmail.com>
2022-05-25 12:27:17 +0430

    style: :lipstick: change searchbar width

--- components/header/SearchBar.tsx
+++ components/header/SearchBar.tsx
@@ -11,7 +11,7 @@ const SearchBar = () => {
   const t = locale === "en" ? en : fa;
   const IconName = "GoSearch";
   return (
-    <div className="max-w-[40rem] w-[70%] md:w-[80%] px-4 rounded-lg bg-gray-200 flex items-center flex-grow">
+    <div className="max-w-[40rem] w-[70%] md:w-[90%] px-4 rounded-lg bg-gray-200 flex items-center flex-grow">
       <Icon iconName={IconName} className="text-gray-400" />
       <input
         className="px-4 py-2 bg-transparent outline-none w-full "
