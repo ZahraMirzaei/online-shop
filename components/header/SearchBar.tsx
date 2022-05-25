84fbc591d2b74c5061189cf631292771bfa60e88
ZahraMirzaei.JS <z.mirzaei2019@gmail.com>
2022-05-25 21:14:53 +0430

    feat: :sparkles: replace react-icons icon instead of Icon component

--- components/header/SearchBar.tsx
+++ components/header/SearchBar.tsx
@@ -1,7 +1,7 @@
 import React from "react";
 
 import { useRouter } from "next/router";
-import Icon from "../UI/Icon";
+import { GoSearch } from "react-icons/go";
 
 import en from "../../locales/pages/home/en";
 import fa from "../../locales/pages/home/fa";
@@ -12,7 +12,7 @@ const SearchBar = () => {
   const IconName = "GoSearch";
   return (
     <div className="max-w-[40rem] w-[70%] md:w-[90%] px-4 rounded-lg bg-gray-200 flex items-center flex-grow">
-      <Icon iconName={IconName} className="text-gray-400" />
+      <GoSearch style={{ color: "rgb(156 163 175)" }} />
       <input
         className="px-4 py-2 bg-transparent outline-none w-full "
         type="search"
