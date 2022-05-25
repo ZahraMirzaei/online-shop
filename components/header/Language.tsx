7384d833dc3efcf7355825bc45a3671ae1579ee8
ZahraMirzaei.JS <z.mirzaei2019@gmail.com>
2022-05-25 20:19:07 +0430

    feat: :sparkles: change language appearance and make it like button

--- components/header/Language.tsx
+++ components/header/Language.tsx
@@ -2,6 +2,7 @@ import React, { useEffect, useState } from "react";
 import { useRouter } from "next/router";
 import Link from "next/link";
 import { MdLanguage } from "react-icons/md";
+import { HiChevronUp, HiChevronDown } from "react-icons/hi";
 import en from "../../locales/en";
 import fa from "../../locales/fa";
 const Language = () => {
@@ -10,6 +11,7 @@ const Language = () => {
   const [lang, setLang] = useState(locale);
 
   const [openLang, setOpenLang] = useState(false);
+  let ArrowDirection = !openLang ? HiChevronDown : HiChevronUp;
 
   useEffect(() => {
     document.documentElement.dir = locale === "en" ? "ltr" : "rtl";
@@ -21,10 +23,22 @@ const Language = () => {
 
   return (
     <div className=" relative">
-      <MdLanguage
-        style={{ fontSize: "1.4rem", margin: "0 0.1rem 0 0.1rem" }}
+      <div
+        className="flex items-center cursor-pointer"
         onClick={() => setOpenLang((prevState) => !prevState)}
-      />
+      >
+        <div className="flex items-center md:border-2 border-palette-secondary rounded-lg py-1 md:px-2 shadow-md">
+          <MdLanguage
+            style={{ fontSize: "1.4rem", margin: "0 0.1rem 0 0.1rem" }}
+          />
+          <p className="hidden md:block mx-[0.15rem] text-xs font-bold">
+            {locale === "en" ? "English" : "فارسی"}
+          </p>
+          <div className="hidden md:block ">
+            <ArrowDirection />
+          </div>
+        </div>
+      </div>
       {openLang ? (
         <>
           <div
@@ -32,7 +46,7 @@ const Language = () => {
             onClick={() => setOpenLang(false)}
           ></div>
           <div
-            className={`absolute top-8 ltr:right-0 rtl:left-0 bg-palette-card py-3 px-6 shadow-md rounded-md`}
+            className={`absolute top-9 ltr:right-0 rtl:left-0 bg-palette-card py-3 px-6 shadow-md rounded-md`}
           >
             <Link href="/" locale="fa">
               <a className="whitespace-nowrap flex justify-between items-center">
