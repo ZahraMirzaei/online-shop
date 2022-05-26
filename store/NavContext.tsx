10dfa359bee275bdf804df4252572dd06ea40202
ZahraMirzaei.JS <z.mirzaei2019@gmail.com>
2022-05-26 11:13:03 +0430

    feat: :sparkles: add NavContext to use in SideNav
    it should hold the products group of each category and use it in SideNavSide and DropDown components

---
+++ store/NavContext.tsx
@@ -0,0 +1,51 @@
+import React, { createContext, useState } from "react";
+import { IDropDown } from "../lib/interface/dropDown";
+
+type SidebarContextObj = {
+  isOpen: boolean;
+  dropDownList: IDropDown[];
+  setDropDownList: (entries: IDropDown[]) => void;
+  openSidebar: () => void;
+  closeSidebar: () => void;
+};
+
+const SidebarContext = createContext<SidebarContextObj>({
+  isOpen: false,
+  dropDownList: [],
+  setDropDownList: (entries: IDropDown[]) => {},
+  openSidebar: () => {},
+  closeSidebar: () => {},
+});
+
+export const SidebarContextProvider: React.FC<{ children: React.ReactNode }> = (
+  props
+) => {
+  const [isOpen, setIsOpen] = useState(false);
+  const [sidebarContent, setSidebarContent] = useState<IDropDown[]>([]);
+
+  function openSidebar() {
+    setIsOpen(true);
+  }
+
+  function closeSidebar() {
+    setIsOpen(false);
+  }
+
+  function setSidebarEntries(entries: IDropDown[]) {
+    setSidebarContent(entries);
+  }
+
+  const contextValue: SidebarContextObj = {
+    isOpen,
+    dropDownList: sidebarContent,
+    setDropDownList: setSidebarEntries,
+    openSidebar,
+    closeSidebar,
+  };
+
+  return (
+    <SidebarContext.Provider value={contextValue}>
+      {props.children}
+    </SidebarContext.Provider>
+  );
+};
