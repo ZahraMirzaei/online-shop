7674d023c4193c2b5bd9398cc37d496b6282250f
ZahraMirzaei.JS <z.mirzaei2019@gmail.com>
2022-05-26 19:25:48 +0430

    feat: :sparkles: add sidebarside to display sub category

---
+++ components/header/SideNav/SideNavSide.tsx
@@ -0,0 +1,68 @@
+import React, { useRef, useContext } from "react";
+import { useRouter } from "next/router";
+import { Transition } from "react-transition-group";
+import { HiOutlineArrowSmRight, HiOutlineArrowSmLeft } from "react-icons/hi";
+import DropDown from "../../UI/DropDown";
+import SidebarContext from "../../../store/NavContext";
+import en from "../../../locales/en";
+import fa from "../../../locales/fa";
+
+const SideNavSide = () => {
+  const sidebarCtx = useContext(SidebarContext);
+  const nodeRef = useRef<HTMLDivElement>(null);
+  const { locale } = useRouter();
+  const t = locale === "en" ? en : fa;
+  const BackArrow =
+    locale === "en" ? HiOutlineArrowSmLeft : HiOutlineArrowSmRight;
+
+  return (
+    <>
+      {sidebarCtx.dropDownList.length ? (
+        <Transition
+          nodeRef={nodeRef}
+          in={sidebarCtx.isOpen}
+          timeout={300}
+          mountOnEnter
+          unmountOnExit
+        >
+          {(state) => {
+            return (
+              <div
+                className={`max-w-[380px] w-[80%] h-screen fixed top-0 shadow-md z-[110] bg-palette-card origin-left overflow-auto md:hidden
+                ${locale == "en" ? "left-0" : "right-0"} 
+                
+                ${
+                  state === "entering"
+                    ? "ltr:animate-sidenavLTREntering rtl:animate-sidenavRTLEntering"
+                    : state === "entered"
+                    ? "translate-x-0"
+                    : "ltr:animate-sidenavLTRExit rtl:animate-sidenavRTLExit"
+                }
+                `}
+              >
+                <div
+                  className="flex items-center pt-4 pb-3 px-6 font-bold text-lg"
+                  onClick={sidebarCtx.closeSidebar}
+                >
+                  <BackArrow style={{ fontSize: "1.5rem" }} />
+                  <h3 className="ltr:ml-2 rtl:mr-2 py-1">{t.mainMenu}</h3>
+                </div>
+                <hr className="mb-6" />
+                {sidebarCtx.dropDownList.map((item) => {
+                  return (
+                    <div key={item.title}>
+                      <DropDown dropDown={item} ref={nodeRef} />
+                    </div>
+                  );
+                })}
+              </div>
+            );
+          }}
+        </Transition>
+      ) : null}
+    </>
+  );
+};
+
+SideNavSide.displayName = "SideNavSide";
+export default SideNavSide;
