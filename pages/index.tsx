97fc2253ae0696fb0c51eea22d330e7b5da9ae96
ZahraMirzaei.JS <z.mirzaei2019@gmail.com>
2022-05-26 19:28:30 +0430

    feat: :sparkles: make index page clear

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,11 +1,7 @@
 import type { NextPage } from "next";
 
 const Home: NextPage = () => {
-  return (
-    <div className="flex flex-col items-center justify-center w-full h-screen">
-      <h1 className="text-3xl">Online shop project started</h1>
-    </div>
-  );
+  return <></>;
 };
 
 export default Home;
