3e12a90c3d1062c573d69b3d4af099b25d781bb3
ZahraMirzaei.JS <z.mirzaei2019@gmail.com>
2022-05-27 21:56:05 +0430

    feat: :sparkles: add extraMenu to sideNaveContent

--- components/header/SideNav/SideNavContent.tsx
+++ components/header/SideNav/SideNavContent.tsx
@@ -1,7 +1,9 @@
 import React, { useContext } from "react";
+import Link from "next/link";
 import { useLanguage } from "../../../hooks/useLanguage";
 import { HiChevronRight, HiChevronLeft } from "react-icons/hi";
 import menuItems from "../../../mock/menuItems";
+import { extraMenu } from "../../../mock/menuItems";
 import SidebarContext from "../../../store/NavContext";
 import { IDropDown } from "../../../lib/interface/dropDown";
 
@@ -15,11 +17,30 @@ const SideNavContent = () => {
   };
   return (
     <div className="absolute w-full h-full overflow-y-auto">
+      <div className="flex flex-col  mt-3 pt-3  ltr:px-5 rtl:px-5  cursor-pointer">
+        {extraMenu.map((menuItem) => {
+          return (
+            <div
+              className="flex items-center  py-3 text-palette-mute "
+              key={menuItem.title}
+            >
+              <menuItem.icon />
+              <Link href="/">
+                <a className=" mx-4">{t[`${menuItem.title}`]}</a>
+              </Link>
+            </div>
+          );
+        })}
+        <hr className="mt-6 mb-4 border-gray-200" />
+      </div>
+      <h2 className="font-semibold text-lg py-3 ltr:px-5 rtl:px-5">
+        {t.CategoryOfGoods}
+      </h2>
       {menuItems.map((item) => {
         return (
           <div key={item.category}>
             <div
-              className="flex items-center mt-3 py-3 ltr:px-5 rtl:px-5  cursor-pointer font-semibold hover:ltr:md:translate-x-2 hover:rtl:md:-translate-x-2 transition-transform"
+              className="flex items-center mt-3 py-3 ltr:px-5 rtl:px-5  cursor-pointer  hover:ltr:md:translate-x-2 hover:rtl:md:-translate-x-2 transition-transform"
               onClick={() => openNav(item.productsGroup)}
             >
               <item.icon className="w-6 h-6 " />
