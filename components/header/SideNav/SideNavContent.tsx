04ff8f26257ec12fad2b79213300872db44165f2
ZahraMirzaei.JS <z.mirzaei2019@gmail.com>
2022-05-27 11:30:43 +0430

    style: :lipstick: cahnge space in sidenav

--- components/header/SideNav/SideNavContent.tsx
+++ components/header/SideNav/SideNavContent.tsx
@@ -11,7 +11,7 @@ const SideNavContent = () => {
   const { locale } = useRouter();
   const t = locale === "en" ? en : fa;
   const sideNavCtx = useContext(SidebarContext);
-
+  const ArrowDirection = locale === "en" ? HiChevronRight : HiChevronLeft;
   const openNav = (sidebarSideContent: IDropDown[] = []) => {
     sideNavCtx.setDropDownList(sidebarSideContent);
     sideNavCtx.openSidebar();
@@ -22,23 +22,19 @@ const SideNavContent = () => {
         return (
           <div key={item.category}>
             <div
-              className="flex items-center mt-3 py-3 ltr:pl-5 rtl:pr-5  cursor-pointer font-semibold hover:ltr:md:translate-x-2 hover:rtl:md:-translate-x-2 transition-transform"
+              className="flex items-center mt-3 py-3 ltr:px-5 rtl:px-5  cursor-pointer font-semibold hover:ltr:md:translate-x-2 hover:rtl:md:-translate-x-2 transition-transform"
               onClick={() => openNav(item.productsGroup)}
             >
               <item.icon className="w-6 h-6 " />
               <div
-                className={`mx-4 ${
+                className={`mx-4 grow ${
                   !item.productsGroup ? "text-gray-400 font-normal" : ""
                 }`}
               >
                 {t[item.category]}
               </div>
               {item.productsGroup ? (
-                locale === "en" ? (
-                  <HiChevronRight />
-                ) : (
-                  <HiChevronLeft />
-                )
+                <ArrowDirection style={{ fontSize: "1.5rem" }} />
               ) : null}
             </div>
           </div>
