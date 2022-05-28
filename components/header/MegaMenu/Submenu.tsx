1eb7ffb15ebf0d0b5cdc312e7f61da40e99db09c
ZahraMirzaei.JS <z.mirzaei2019@gmail.com>
2022-05-28 17:25:09 +0430

    feat: :sparkles: add subMenu to megamenu

---
+++ components/header/MegaMenu/Submenu.tsx
@@ -0,0 +1,43 @@
+import React from "react";
+import Link from "next/link";
+import { useLanguage } from "../../../hooks/useLanguage";
+import { IDropDown } from "../../../lib/interface/dropDown";
+
+interface Props {
+  subMenu: IDropDown[] | undefined;
+}
+const Submenu: React.FC<Props> = ({ subMenu }) => {
+  const { t } = useLanguage();
+  return (
+    <div className="relative grow md:columns-[188px]  xl:columns-3 xl:max-w-4xl  px-6 py-5  ">
+      {subMenu ? (
+        subMenu.map((item, index) => {
+          return (
+            <div className="py-3" key={`${item}-${index}`}>
+              <Link href="/">
+                <a className="block text-sm rtl:ml-10 ltr:mr-10 font-bold px-2 ltr:border-l-4 rtl:border-r-4 border-palette-primary rounded-sm">
+                  {t[`${item.title}`]}
+                </a>
+              </Link>
+              {item.subtitles.map((item, index) => {
+                return (
+                  <div key={`${item}-${index}`}>
+                    <Link href="/">
+                      <a className="block py-2">{t[`${item}`]}</a>
+                    </Link>
+                  </div>
+                );
+              })}
+            </div>
+          );
+        })
+      ) : (
+        <p className="text-sm text-palette-mute absolute top-[45%] ltr:left-[30%] rtl:right-[30%]">
+          {t.noProduct}
+        </p>
+      )}
+    </div>
+  );
+};
+
+export default Submenu;
