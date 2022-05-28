27e0a29102c496912a77d853c6fa1bb7b6c66fa1
ZahraMirzaei.JS <z.mirzaei2019@gmail.com>
2022-05-28 10:03:37 +0430

    feat: :sparkles: add new component to dispaly ExtraMenu next to megamenu

---
+++ components/header/MegaMenu/ExtraMenu.tsx
@@ -0,0 +1,27 @@
+import React from "react";
+import Link from "next/link";
+import { extraMenu } from "../../../mock/menuItems";
+import { useLanguage } from "../../../hooks/useLanguage";
+
+const ExtraMenu = () => {
+  const { t } = useLanguage();
+  return (
+    <div className="flex items-center ltr:border-l-2 rtl:border-r-2 border-gray-300 grow rtl:mx-5 ltr:ml-2">
+      {extraMenu.map((menuItem) => {
+        return (
+          <div
+            className="flex items-center text-palette-mute mx-2"
+            key={menuItem.title}
+          >
+            <menuItem.icon />
+            <Link href="/">
+              <a>{t[`${menuItem.title}`]}</a>
+            </Link>
+          </div>
+        );
+      })}
+    </div>
+  );
+};
+
+export default ExtraMenu;
