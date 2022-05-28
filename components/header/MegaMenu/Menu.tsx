4d0bef44a1eaeeb7c582a568bb55ec11b52d6cdc
ZahraMirzaei.JS <z.mirzaei2019@gmail.com>
2022-05-28 17:27:59 +0430

    feat: :sparkles: add MenuItems and Submenu components to Menu component

--- components/header/MegaMenu/Menu.tsx
+++ components/header/MegaMenu/Menu.tsx
@@ -1,23 +1,25 @@
-import React from "react";
-import Link from "next/link";
-import { useLanguage } from "../../../hooks/useLanguage";
+import React, { useState, useEffect } from "react";
 import menuItems from "../../../mock/menuItems";
+import MenuItems from "../../UI/MenuItems";
+import { IDropDown } from "../../../lib/interface/dropDown";
+import Submenu from "./Submenu";
 const Menu = () => {
-  const { t } = useLanguage();
+  const [submenu, setSubmenu] = useState<IDropDown[]>();
+
+  function activeItem(submenuList: IDropDown[] | undefined) {
+    setSubmenu(submenuList);
+  }
+  useEffect(() => {
+    setSubmenu(menuItems[0].productsGroup);
+  }, []);
+
   return (
-    <nav>
-      <ul>
-        {menuItems.map((catItem) => {
-          return (
-            <li key={catItem.category}>
-              <Link href="/">
-                <a>{t[`${catItem.category}`]}</a>
-              </Link>
-            </li>
-          );
-        })}
-      </ul>
-    </nav>
+    <div className=" flex">
+      <nav className="md:w-72 md:py-4 ltr:border-r-2 rtl:border-l-2 border-slate-300">
+        <MenuItems onMouseOver={activeItem} />
+      </nav>
+      <Submenu subMenu={submenu} />
+    </div>
   );
 };
 
