a1245641ac631d8f97560c98884d56b52e9c658a
ZahraMirzaei.JS <z.mirzaei2019@gmail.com>
2022-06-01 20:33:25 +0430

    feat: :sparkles: add custom arrow component to carousel
    use in carousel/index file in setting object of Slider component

---
+++ components/carousel/Arrows.tsx
@@ -0,0 +1,25 @@
+import React from "react";
+
+interface Props {
+  className?: string;
+  style?: any;
+  onClick?: () => void;
+}
+export const NextArrow: React.FC<Props> = ({ className, style, onClick }) => {
+  return (
+    <div
+      className={`${className}  z-10 w-28 h-full !flex items-center justify-center ltr:left-auto ltr:right-0 rtl:right-auto rtl:left-0 before:text-[30px] before:content-['→'] hover:bg-palette-card/10`}
+      style={{ ...style }}
+      onClick={onClick}
+    />
+  );
+};
+export const PrevArrow: React.FC<Props> = ({ className, style, onClick }) => {
+  return (
+    <div
+      className={`${className} z-10 w-28 h-full !flex items-center justify-center ltr:left-0 ltr:right-auto rtl:right-0 rtl:left-auto before:text-[30px] before:content-['←'] hover:bg-palette-card/10`}
+      style={{ ...style }}
+      onClick={onClick}
+    />
+  );
+};
