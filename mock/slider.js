c4fe24b2bc9dff33e94cd1a5587e3cdd952406a7
ZahraMirzaei.JS <z.mirzaei2019@gmail.com>
2022-06-01 14:20:42 +0430

    feat: :clown_face: add slider mock

---
+++ mock/slider.js
@@ -0,0 +1,44 @@
+export const sliderContent = [
+  {
+    ID: 1,
+    title: "digitalBT",
+    description: "digitalBD",
+    imgSrc: "/images/slider-img/digital-banner.jpg",
+    url: "/",
+  },
+  {
+    ID: 2,
+    title: "stationeryBT",
+    description: "stationeryBD",
+    imgSrc: "/images/slider-img/stationery-banner.jpg",
+    url: "/",
+  },
+  {
+    ID: 3,
+    title: "toyBT",
+    description: "toyBD",
+    imgSrc: "/images/slider-img/toy-banner.jpg",
+    url: "/",
+  },
+  {
+    ID: 4,
+    title: "houseBT",
+    description: "houseBD",
+    imgSrc: "/images/slider-img/house-banner.jpg",
+    url: "/",
+  },
+  {
+    ID: 5,
+    title: "fashionBT",
+    description: "fashionBD",
+    imgSrc: "/images/slider-img/fashion-banner.jpg",
+    url: "/",
+  },
+  {
+    ID: 6,
+    title: "beautyBT",
+    description: "beautyBD",
+    imgSrc: "/images/slider-img/beauty-banner.jpg",
+    url: "/",
+  },
+];
