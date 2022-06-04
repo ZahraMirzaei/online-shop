861d3058ab62bd434e130bbd24a72cf956abb510
ZahraMirzaei.JS <z.mirzaei2019@gmail.com>
2022-06-04 15:43:15 +0430

    feat: :sparkles: import Benefit component to Home page

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,10 +1,12 @@
 import type { NextPage } from "next";
+import Benefits from "../components/Benefits";
 import Carousel from "../components/carousel";
 
 const Home: NextPage = () => {
   return (
     <div>
       <Carousel />
+      <Benefits />
     </div>
   );
 };
