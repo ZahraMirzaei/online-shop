6feef9fbd1b0cf103e8718762080d3bac6605d8c
ZahraMirzaei.JS <z.mirzaei2019@gmail.com>
2022-06-04 16:16:35 +0430

    feat: :clown_face: add benefits mock file

---
+++ mock/benefits.js
@@ -0,0 +1,22 @@
+export const benefitContent = [
+  {
+    imgSrc: "/images/benefit-icons/005-delivery-truck-1.png",
+    title: "deliver",
+  },
+  {
+    imgSrc: "/images/benefit-icons/003-cash-on-delivery.png",
+    title: "cash",
+  },
+  {
+    imgSrc: "/images/benefit-icons/004-headphones.png",
+    title: "support",
+  },
+  {
+    imgSrc: "/images/benefit-icons/006-best-seller.png",
+    title: "warranty",
+  },
+  {
+    imgSrc: "/images/benefit-icons/004-return.png",
+    title: "return",
+  },
+];
