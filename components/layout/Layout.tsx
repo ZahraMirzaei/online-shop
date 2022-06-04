c85eb5af5416682b55c861eaace5ed40e9cb62b2
ZahraMirzaei.JS <z.mirzaei2019@gmail.com>
2022-06-04 19:58:49 +0430

    feat: :sparkles: create layout component

---
+++ components/layout/Layout.tsx
@@ -0,0 +1,24 @@
+import React from "react";
+import Head from "next/head";
+import { ThemeProvider } from "next-themes";
+import Header from "../header";
+import { SidebarContextProvider } from "../../store/NavContext";
+
+const Layout: React.FC<{ children?: React.ReactNode }> = ({ children }) => {
+  return (
+    <ThemeProvider enableSystem={true} attribute="class">
+      <Head>
+        <title>ZShop e-commerce</title>
+      </Head>
+      <header>
+        <SidebarContextProvider>
+          <Header />
+        </SidebarContextProvider>
+      </header>
+      <main>{children}</main>
+      <footer>footer</footer>
+    </ThemeProvider>
+  );
+};
+
+export default Layout;
