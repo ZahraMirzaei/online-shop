af9447213d98d4491d4ca5d9e5d882fd974684ef
ZahraMirzaei.JS <z.mirzaei2019@gmail.com>
2022-06-04 13:37:19 +0430

    style: :lipstick: change carousel arrows's style

--- components/carousel/Arrows.tsx
+++ components/carousel/Arrows.tsx
@@ -8,7 +8,7 @@ interface Props {
 export const NextArrow: React.FC<Props> = ({ className, style, onClick }) => {
   return (
     <div
-      className={`${className}  z-10 w-28 h-full !flex items-center justify-center ltr:left-auto ltr:right-0 rtl:right-auto rtl:left-0 before:text-[30px] before:content-['→'] hover:bg-palette-card/10`}
+      className={`${className} z-10 w-10 lg:w-28 h-full pt-10 !flex items-center justify-center ltr:left-auto ltr:right-0 rtl:right-auto rtl:left-0 before:text-[20px] lg:before:text-[30px] before:content-['→'] hover:bg-palette-card/10`}
       style={{ ...style }}
       onClick={onClick}
     />
@@ -17,7 +17,7 @@ export const NextArrow: React.FC<Props> = ({ className, style, onClick }) => {
 export const PrevArrow: React.FC<Props> = ({ className, style, onClick }) => {
   return (
     <div
-      className={`${className} z-10 w-28 h-full !flex items-center justify-center ltr:left-0 ltr:right-auto rtl:right-0 rtl:left-auto before:text-[30px] before:content-['←'] hover:bg-palette-card/10`}
+      className={`${className} z-10 w-10 lg:w-28 h-full pt-10 !flex items-center justify-center ltr:left-0 ltr:right-auto rtl:right-0 rtl:left-auto before:text-[20px] lg:before:text-[30px] before:content-['←'] hover:bg-palette-card/10`}
       style={{ ...style }}
       onClick={onClick}
     />
