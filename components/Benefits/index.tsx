a51eed177f299c2aff819f336ffe3fca52afe0ed
ZahraMirzaei.JS <z.mirzaei2019@gmail.com>
2022-06-04 16:55:00 +0430

    feat: :sparkles: complate Benefit component

--- components/Benefits/index.tsx
+++ components/Benefits/index.tsx
@@ -1,7 +1,33 @@
 import React from "react";
+import Image from "next/image";
+import { useLanguage } from "../../hooks/useLanguage";
+import { benefitContent } from "../../mock/benefits";
 
 const Benefits = () => {
-  return <div>Benefit</div>;
+  const { t } = useLanguage();
+  return (
+    <div className="flex items-center flex-wrap justify-evenly my-6 md:my-4">
+      {benefitContent.map((benefitItem) => {
+        return (
+          <div
+            className="flex flex-col items-center min-w-[150px] max-w-[250px] py-3 px-2"
+            key={benefitItem.title}
+          >
+            <Image
+              height={48}
+              width={48}
+              src={benefitItem.imgSrc}
+              alt={benefitItem.title}
+              className=""
+            />
+            <p className="py-2 text-[0.75rem] text-palette-mute">
+              {t[`${benefitItem.title}`]}
+            </p>
+          </div>
+        );
+      })}
+    </div>
+  );
 };
 
 export default Benefits;
