2808782339d19bd052cb2427d8c953ac8792e8a3
ZahraMirzaei.JS <z.mirzaei2019@gmail.com>
2022-06-06 10:21:36 +0430

    feat: :sparkles: add getServerSideProps function to Home page and get products and categories from sanity

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,8 +1,17 @@
 import type { NextPage } from "next";
+
+import { client } from "../lib/client";
+
 import Benefits from "../components/Benefits";
 import Carousel from "../components/carousel";
 
-const Home: NextPage = () => {
+import { IProduct } from "../lib/interface/products";
+import { ICategory } from "../lib/interface/categories";
+
+const Home: NextPage<{ products: IProduct[]; categories: ICategory[] }> = ({
+  products,
+  categories,
+}) => {
   return (
     <div>
       <Carousel />
@@ -12,3 +21,17 @@ const Home: NextPage = () => {
 };
 
 export default Home;
+
+export const getServerSideProps = async () => {
+  const productQuery = `*[_type=='product']`;
+  const products = await client.fetch(productQuery);
+
+  const categoryQuery = `*[_type=='category']`;
+  const categories = await client.fetch(categoryQuery);
+  return {
+    props: {
+      products,
+      categories,
+    },
+  };
+};
