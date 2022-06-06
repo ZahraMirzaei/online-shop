a14419522cf478751eb4bf8719986f169fae4211
ZahraMirzaei.JS <z.mirzaei2019@gmail.com>
2022-06-06 10:19:26 +0430

    feat: :label: add IProduct interface for array of product that recieve from sanity

---
+++ lib/interface/products.ts
@@ -0,0 +1,46 @@
+export interface IProductDetails {
+  processor?: string;
+  screen?: string;
+  operating_system?: string;
+  ram?: string;
+  ssd?: string;
+  port?: string;
+  graphic?: string;
+  warranty?: string;
+  back_camera?: string;
+  front_camera?: string;
+  battery?: string;
+}
+
+export type TSlug = {
+  _type: string;
+  current: string;
+};
+
+export type TCategory = {
+  _key: string;
+  _ref: string;
+  _type: "reference";
+};
+
+export type TImage = {
+  _key: string;
+  _type: "image";
+  asset: {
+    _ref: string;
+    _type: "reference";
+  };
+};
+
+export interface IProduct {
+  image: TImage;
+  name: string;
+  slug: TSlug;
+  price: number;
+  discount?: number;
+  details: IProductDetails[];
+  brand: string;
+  category: TCategory[];
+  isOffer: boolean;
+  registerDate: string;
+}
