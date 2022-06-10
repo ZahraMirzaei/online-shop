54430e4b0c79561fd53c9457e9499f5e859272d4
ZahraMirzaei.JS <z.mirzaei2019@gmail.com>
2022-06-10 12:40:09 +0430

    feat: :sparkles: add CarouselBoxArrows component
    to use it in CarouselBox component

---
+++ components/UI/CarouselBox/CarouselBoxArrows.tsx
@@ -0,0 +1,25 @@
+import React from "react";
+
+interface Props {
+  className?: string;
+  style?: any;
+  onClick?: () => void;
+}
+export const NextArrow: React.FC<Props> = ({ className, style, onClick }) => {
+  return (
+    <div
+      className={`${className} z-10 w-14 h-full !flex items-center justify-center ltr:left-auto ltr:-right-2 rtl:right-auto rtl:-left-2 before:text-[20px] lg:before:text-[40px] before:content-['→'] hover:bg-palette-card/20`}
+      style={{ ...style }}
+      onClick={onClick}
+    />
+  );
+};
+export const PrevArrow: React.FC<Props> = ({ className, style, onClick }) => {
+  return (
+    <div
+      className={`${className} z-10 w-14 h-full !flex items-center justify-center ltr:-left-5 ltr:right-auto rtl:-right-5 rtl:left-auto before:text-[20px] lg:before:text-[40px] before:content-['←'] hover:bg-palette-card/20`}
+      style={{ ...style }}
+      onClick={onClick}
+    />
+  );
+};
