bdb46197829b3aab11e7ae8564e877985058e7da
ZahraMirzaei.JS <z.mirzaei2019@gmail.com>
2022-06-10 13:24:51 +0430

    feat: :sparkles: add Offets component
    with CarouselBox component in it

---
+++ components/Offers/Offers.tsx
@@ -0,0 +1,12 @@
+import React from "react";
+import CarouselBox from "../UI/CarouselBox/CarouselBox";
+
+const Offers = () => {
+  return (
+    <div>
+      <CarouselBox title="offers" className="bg-offersBG" />
+    </div>
+  );
+};
+
+export default Offers;
