41861b92006346416691a102cc6afae0f086a20a
ZahraMirzaei.JS <z.mirzaei2019@gmail.com>
2022-06-11 16:49:58 +0430

    feat: :sparkles: add redux provider to layout component

--- components/layout/Layout.tsx
+++ components/layout/Layout.tsx
@@ -1,23 +1,27 @@
 import React from "react";
+import { Provider } from "react-redux";
 import Head from "next/head";
 import { ThemeProvider } from "next-themes";
 import Header from "../header";
 import { SidebarContextProvider } from "../../store/context/NavContext";
+import store from "../../store/index";
 
 const Layout: React.FC<{ children?: React.ReactNode }> = ({ children }) => {
   return (
-    <ThemeProvider enableSystem={true} attribute="class">
-      <Head>
-        <title>ZiShop e-commerce</title>
-      </Head>
-      <header>
-        <SidebarContextProvider>
-          <Header />
-        </SidebarContextProvider>
-      </header>
-      <main>{children}</main>
-      <footer>footer</footer>
-    </ThemeProvider>
+    <Provider store={store}>
+      <ThemeProvider enableSystem={true} attribute="class">
+        <Head>
+          <title>ZiShop e-commerce</title>
+        </Head>
+        <header>
+          <SidebarContextProvider>
+            <Header />
+          </SidebarContextProvider>
+        </header>
+        <main>{children}</main>
+        <footer>footer</footer>
+      </ThemeProvider>
+    </Provider>
   );
 };
 
