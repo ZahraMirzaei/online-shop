385c0b5fd84b285b0092edb21a4bc27f0fb6997f
ZahraMirzaei.JS <z.mirzaei2019@gmail.com>
2022-06-11 17:07:37 +0430

    feat: :sparkles: get offer products from redux and send it to Carousel Box

--- components/Offers/Offers.tsx
+++ components/Offers/Offers.tsx
@@ -1,10 +1,19 @@
 import React from "react";
 import CarouselBox from "../UI/CarouselBox/CarouselBox";
+import { useSelector } from "react-redux";
 
 const Offers = () => {
+  const OfferProducts = useSelector(
+    (state: any) => state.specialOfferProductsList.specialOfferProducts
+  );
+
   return (
     <div>
-      <CarouselBox title="offers" className="bg-offersBG" />
+      <CarouselBox
+        title="offers"
+        className="bg-offersBG"
+        productList={OfferProducts}
+      />
     </div>
   );
 };
