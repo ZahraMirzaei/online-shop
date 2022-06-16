8931c95beacf62bc1628097facb5b693ab063d0c
ZahraMirzaei.JS <z.mirzaei2019@gmail.com>
2022-06-16 13:19:11 +0430

    feat: :clown_face: add category-sm mock
    it has categories information for sm break point

---
+++ mock/category-sm.js
@@ -0,0 +1,44 @@
+export const categorySmContent = [
+  {
+    bgc: "digitalCategory",
+    imgSrc: "digital-category.png",
+    categoryTitle: "digitalCategoryTitle",
+    href: "/",
+  },
+  {
+    bgc: "fashionCategory",
+    imgSrc: "fashion-category.png",
+    categoryTitle: "fashionCategoryTitle",
+    href: "/",
+  },
+  {
+    bgc: "beautyCategory",
+    imgSrc: "beauty-category.png",
+    categoryTitle: "beautyCategoryTitle",
+    href: "/",
+  },
+  {
+    bgc: "sportCategory",
+    imgSrc: "sport-category.png",
+    categoryTitle: "sportCategoryTitle",
+    href: "/",
+  },
+  {
+    bgc: "houseCategory",
+    imgSrc: "house-category.png",
+    categoryTitle: "houseCategoryTitle",
+    href: "/",
+  },
+  {
+    bgc: "toyCategory",
+    imgSrc: "toy-category.png",
+    categoryTitle: "toyCategoryTitle",
+    href: "/",
+  },
+  {
+    bgc: "stationeryCategory",
+    imgSrc: "stationery-category.png",
+    categoryTitle: "stationeryCategoryTitle",
+    href: "/",
+  },
+];
