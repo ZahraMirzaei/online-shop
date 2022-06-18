14f341bc345e76ca9a78b2f2d6b69681d1fb3e0b
ZahraMirzaei.JS <z.mirzaei2019@gmail.com>
2022-06-18 17:22:24 +0430

    feat: :sparkles: make card reusable and change its styles

--- components/UI/Card.tsx
+++ components/UI/Card.tsx
@@ -5,58 +5,101 @@ import { useLanguage } from "../../hooks/useLanguage";
 import { AiOutlineHeart, AiOutlineShoppingCart } from "react-icons/ai";
 import { IoShareSocialOutline } from "react-icons/io5";
 import StarRatingComponent from "react-star-rating-component";
+import { IProduct } from "../../lib/interface/products";
+import { urlFor } from "../../lib/client";
+import {
+  gbpCurrencyFormat,
+  irrCurrencyFormat,
+} from "./CarouselBox/CarouselBoxCard";
 
-const Card = () => {
+interface Props {
+  href: string;
+  product: IProduct;
+}
+
+const Card: React.FC<Props> = ({ href, product }) => {
   const { t, locale } = useLanguage();
   return (
-    <div className="flex justify-start flex-wrap">
-      <div className="max-w-[480px] sm:max-w-[300px] sm:w-[50%] md:min-w-[250px] md:w-[250px] md:max-w-[300px] flex-grow shadow-xl my-4 ltr:mr-2 rtl:ml-1 md:mx-3  bg-palette-card rounded-xl">
-        <Link href="/">
-          <a className="flex md:items-center md:flex-col relative">
-            <div className="w-full bg-slate-400/30 px-1 md:px-6 py-2 rounded-bl-xl rounded-tl-xl md:rounded-tr-xl md:rounded-bl-none rtl:order-2 rtl:md:order-none">
-              <Image
-                width={280}
-                height={250}
-                src="/images/card-temporary/image.png"
-                alt="laptop"
-                className=" drop-shadow-xl object-contain "
-              />
-            </div>
-            <div className="flex flex-col justify-between items-start  w-[90%]  px-1 md:px-3 py-2 md:py-4">
-              <div className="flex justify-center flex-col flex-grow">
-                <div className="self-center">
-                  <StarRatingComponent
-                    name="product_rate"
-                    starCount={5}
-                    value={4}
-                    starColor={"gold"}
-                    emptyStarColor={"gray"}
-                  />
-                </div>
-                <h4 className="text-sm sm:text-[12px] md:text-sm text-center text-palette-mute ltr:md:-ml-4 rtl:md:-mr-4 ">
-                  Lenovo Yoga 7i Intel Core i5 8GB RAM 256GB SSD 14&quot; 2-in-1
-                  Laptop
-                </h4>
-              </div>
-              <span className="text-sm self-end mt-6 font-bold">
-                <sup className="mr-1">{locale === "en" ? "$" : ""}</sup>2,350,00
-                <sub className="ml-1">{locale === "fa" ? "تومان" : ""}</sub>
+    <div className="col-span-6 sm:col-span-3 md:col-span-4 lg:col-span-3 xl:col-span-2 shadow-xl my-4 ltr:mr-2 rtl:ml-1 md:mx-6  bg-palette-card rounded-xl">
+      <Link href={href}>
+        <a className="flex md:items-center md:flex-col relative">
+          <div className="w-full bg-slate-400/30 px-1 md:px-6 py-2 rounded-bl-xl rounded-tl-xl md:rounded-tr-xl md:rounded-bl-none rtl:order-2 rtl:md:order-none">
+            <Image
+              src={urlFor(product?.image[0]).url()}
+              width={280}
+              height={300}
+              alt={product.name}
+              className=" drop-shadow-xl object-contain hover:scale-110 transition-transform !py-2"
+            />
+            {product?.discount ? (
+              <span className="block absolute -top-2 -right-2">
+                <Image
+                  src="/images/discount-icon/discount.png"
+                  width={40}
+                  height={40}
+                  alt="discount-icon"
+                />
               </span>
-              <div className="w-[52%] md:w-auto md:h-[130px] mt-2 p-2 flex md:flex-col justify-around self-center absolute bottom-0 md:bottom-auto md:-top-2 left-0  md:-left-3 rounded-bl-xl md:rounded-full shadow-lg backdrop-filter backdrop-blur-[8px] bg-palette-card/20">
-                <div className="hover:text-rose-600 transition-colors">
-                  <AiOutlineHeart style={{ fontSize: "1.2rem" }} />
-                </div>
-                <div className="hover:text-rose-600 transition-colors">
-                  <IoShareSocialOutline style={{ fontSize: "1.2rem" }} />
-                </div>
-                <div className="hover:text-rose-600 transition-colors">
-                  <AiOutlineShoppingCart style={{ fontSize: "1.2rem" }} />
-                </div>
+            ) : null}
+            <div className="md:w-auto md:h-[130px] mt-2 p-2 flex md:flex-col justify-around self-center md:absolute bottom-0 md:bottom-auto md:-top-2 left-0  md:-left-1 rounded-lg md:rounded-full shadow-lg backdrop-filter backdrop-blur-[8px] bg-palette-card/20">
+              <div className="hover:text-rose-600 transition-colors sm:px-3 md:px-0">
+                <AiOutlineHeart style={{ fontSize: "1.2rem" }} />
+              </div>
+              <div className="hover:text-rose-600 transition-colors sm:px-3 md:px-0">
+                <IoShareSocialOutline style={{ fontSize: "1.2rem" }} />
+              </div>
+              <div className="hover:text-rose-600 transition-colors sm:px-3 md:px-0">
+                <AiOutlineShoppingCart style={{ fontSize: "1.2rem" }} />
               </div>
             </div>
-          </a>
-        </Link>
-      </div>
+          </div>
+          <div className="flex flex-col justify-between  flex-grow  w-[90%]  px-1 md:px-3 py-2 md:py-4">
+            <div className="flex justify-center flex-col  flex-grow overflow-hidden">
+              <div className="self-center">
+                <StarRatingComponent
+                  name="product_rate"
+                  starCount={5}
+                  value={product.starRating}
+                  starColor={"gold"}
+                  emptyStarColor={"gray"}
+                />
+              </div>
+              <h4 className="text-sm sm:text-[12px] md:text-sm text-center text-palette-mute md:h-10">
+                {product.name}
+              </h4>
+            </div>
+            <div className=" ltr:self-start rtl:self-end mt-4 text-left">
+              {product?.discount ? (
+                <span className="flex flex-col">
+                  <del className="text-[10px] md:text-sm text-rose-600">
+                    <sup className="mr-1">{locale === "en" ? "£" : ""}</sup>
+                    {locale === "en"
+                      ? gbpCurrencyFormat(product?.price)
+                      : irrCurrencyFormat(product?.irrprice)}
+
+                    <sub className="ml-1">{locale === "fa" ? "تومان" : ""}</sub>
+                  </del>
+                  <ins className="text-base md:text-[16px] font-bold self-end">
+                    <sup className="mr-1">{locale === "en" ? "£" : ""}</sup>
+                    {locale === "en"
+                      ? gbpCurrencyFormat(product?.discount)
+                      : irrCurrencyFormat(product?.irrdiscount)}
+                    <sub className="ml-1">{locale === "fa" ? "تومان" : ""}</sub>
+                  </ins>
+                </span>
+              ) : (
+                <span className="font-bold underline">
+                  <sup className="mr-1">{locale === "en" ? "£" : ""}</sup>
+                  {locale === "en"
+                    ? gbpCurrencyFormat(product?.price)
+                    : irrCurrencyFormat(product?.irrprice)}
+                  <sub className="ml-1">{locale === "fa" ? "تومان" : ""}</sub>
+                </span>
+              )}
+            </div>
+          </div>
+        </a>
+      </Link>
     </div>
   );
 };
