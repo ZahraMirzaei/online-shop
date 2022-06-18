c25cefcf3683cd1b12812673f4486b442e4c91d9
ZahraMirzaei.JS <z.mirzaei2019@gmail.com>
2022-06-18 13:52:16 +0430

    style: :lipstick: add drop-shadow to carousel arrows

--- components/UI/CarouselBox/CarouselBoxArrows.tsx
+++ components/UI/CarouselBox/CarouselBoxArrows.tsx
@@ -8,7 +8,7 @@ interface Props {
 export const NextArrow: React.FC<Props> = ({ className, style, onClick }) => {
   return (
     <div
-      className={`${className} z-10 w-14 h-full !flex items-center justify-center ltr:left-auto ltr:-right-2 rtl:right-auto rtl:-left-2 before:text-[20px] lg:before:text-[40px] before:content-['→'] hover:bg-palette-card/20`}
+      className={`${className} z-10 w-14 h-full !flex items-center justify-center ltr:left-auto ltr:-right-2 rtl:right-auto rtl:-left-2 before:text-[20px] lg:before:text-[40px] before:content-['→'] hover:bg-palette-card/20 drop-shadow-xl`}
       style={{ ...style }}
       onClick={onClick}
     />
@@ -17,7 +17,7 @@ export const NextArrow: React.FC<Props> = ({ className, style, onClick }) => {
 export const PrevArrow: React.FC<Props> = ({ className, style, onClick }) => {
   return (
     <div
-      className={`${className} z-10 w-14 h-full !flex items-center justify-center ltr:-left-5 ltr:right-auto rtl:-right-5 rtl:left-auto before:text-[20px] lg:before:text-[40px] before:content-['←'] hover:bg-palette-card/20`}
+      className={`${className} z-10 w-14 h-full !flex items-center justify-center ltr:-left-5 ltr:right-auto rtl:-right-5 rtl:left-auto before:text-[20px] lg:before:text-[40px] before:content-['←'] hover:bg-palette-card/20 drop-shadow-lg`}
       style={{ ...style }}
       onClick={onClick}
     />
