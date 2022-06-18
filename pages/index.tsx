bc3a1ebade6074cd8bef52d24474bfe962076efd
ZahraMirzaei.JS <z.mirzaei2019@gmail.com>
2022-06-18 17:27:11 +0430

    feat: :sparkles: add Newest component to Home page

--- pages/index.tsx
+++ pages/index.tsx
@@ -3,6 +3,7 @@ import { useEffect } from "react";
 
 import { useDispatch } from "react-redux";
 import { specialOfferProductsActions } from "../store/specialOfferProducts-slice";
+import { newestProductsActions } from "../store/newestProduct-slice";
 
 import { client } from "../lib/client";
 
@@ -10,6 +11,7 @@ import Benefits from "../components/Benefits";
 import Carousel from "../components/carousel";
 import Offers from "../components/Offers/Offers";
 import Category from "../components/category/Category";
+import Newest from "../components/newest/Newest";
 
 import { IProduct } from "../lib/interface/products";
 import { ICategory } from "../lib/interface/categories";
@@ -19,12 +21,35 @@ const Home: NextPage<{ products: IProduct[]; categories: ICategory[] }> = ({
   categories,
 }) => {
   const dispatch = useDispatch();
-  console.log(products);
 
+  //add products to offers list
   useEffect(() => {
     const offersProducts = products.filter((item) => item.discount);
     dispatch(specialOfferProductsActions.addProducts(offersProducts));
   }, [dispatch, products]);
+
+  //newest products
+  function getTimeStamp(date: string) {
+    const creationProductDate = new Date(date);
+    return creationProductDate.getTime();
+  }
+
+  const sortByTimeStamp = (product1: IProduct, product2: IProduct): number => {
+    if (product2?.timeStamp && product1?.timeStamp) {
+      return product2?.timeStamp - product1?.timeStamp;
+    }
+    return 0;
+  };
+
+  //add products to newest list
+  useEffect(() => {
+    const productsWithTimeStamp = products.map((product) => {
+      return { ...product, timeStamp: getTimeStamp(product.registerDate) };
+    });
+    const sortedProductsByTimeStamp =
+      productsWithTimeStamp.sort(sortByTimeStamp);
+    dispatch(newestProductsActions.addProducts(sortedProductsByTimeStamp));
+  }, [products, dispatch]);
 
   return (
     <div>
@@ -32,6 +57,7 @@ const Home: NextPage<{ products: IProduct[]; categories: ICategory[] }> = ({
       <Benefits />
       <Offers />
       <Category />
+      <Newest />
     </div>
   );
 };
