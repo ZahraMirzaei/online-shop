7a8510652c5f6b1808ad386e1dc5be69fd869e8e
ZahraMirzaei.JS <z.mirzaei2019@gmail.com>
2022-06-18 17:25:41 +0430

    feat: :sparkles: add Newest component
    to show new products section

---
+++ components/newest/Newest.tsx
@@ -0,0 +1,39 @@
+import React from "react";
+import { useSelector } from "react-redux";
+import { useLanguage } from "../../hooks/useLanguage";
+import { useWindowDimensions } from "../../hooks/useWindowDimensions";
+import Link from "next/link";
+import Card from "../UI/Card";
+import { IProduct } from "../../lib/interface/products";
+
+const Newest = () => {
+  const { t } = useLanguage();
+  const { width } = useWindowDimensions();
+  let numProductToShow = width >= 1280 ? 12 : 8;
+
+  const newestProducts = useSelector(
+    (state: any) => state.newestProductsList.newestProducts
+  );
+
+  return (
+    <div className="my-4 mx-auto lg:my-10 flex flex-col max-w-[1650px]">
+      <h2 className="mt-4 mx-auto py-8 md:text-xl lg:text-2xl">{t.newest}</h2>
+
+      <div className="grid gap-4 md:gap-2 grid-cols-6 md:grid-cols-12 max-w-[1700px]">
+        {newestProducts.slice(0, numProductToShow).map((product: IProduct) => {
+          return <Card key={product.name} href="/" product={product} />;
+        })}
+      </div>
+
+      <div className="text-center">
+        <Link href="/">
+          <a className="inline-block py-3 px-12 my-4 bg-palette-primary text-palette-side rounded-xl shadow-lg">
+            {t.seeAllNewProducts}
+          </a>
+        </Link>
+      </div>
+    </div>
+  );
+};
+
+export default Newest;
