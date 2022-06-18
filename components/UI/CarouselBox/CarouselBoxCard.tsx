426817eadb3f2166c949eb0e0e5c773cf0a79db2
ZahraMirzaei.JS <z.mirzaei2019@gmail.com>
2022-06-18 14:10:53 +0430

    feat: :lipstick: add functions to convert price format in IRR and GBP currency- and add discount icon on cards in  CarouselBoxCard component

--- components/UI/CarouselBox/CarouselBoxCard.tsx
+++ components/UI/CarouselBox/CarouselBoxCard.tsx
@@ -1,6 +1,5 @@
-import React, { useState, useEffect } from "react";
+import React from "react";
 import Image from "next/image";
-import Link from "next/link";
 import { useLanguage } from "../../../hooks/useLanguage";
 import { urlFor } from "../../../lib/client";
 import { IProduct } from "../../../lib/interface/products";
@@ -9,69 +8,62 @@ interface Props {
   product: IProduct;
 }
 
+// Rial currency format
+export const irrCurrencyFormat = (price: number | undefined) => {
+  return price ? new Intl.NumberFormat("fa-IR").format(price) : null;
+};
+
+//pound currency format
+export const gbpCurrencyFormat = (price: number | undefined) => {
+  return price ? new Intl.NumberFormat("en-GB").format(price) : null;
+};
+
 const CarouselBoxCard: React.FC<Props> = ({ product }) => {
   const { locale } = useLanguage();
-  const [IRRPrice, setIRRPrice] = useState<string>();
-  const [IRRDiscount, setIRRDiscount] = useState<string>();
-
-  {
-    /*useEffects do its job just if locale === 'fa' */
-  }
-  useEffect(() => {
-    if (locale === "fa" && product?.irrprice) {
-      let formattedPrice = new Intl.NumberFormat("fa-IR").format(
-        //convert to farsi format
-        product?.irrprice
-      );
-      setIRRPrice(formattedPrice);
-    }
-  }, [locale, product?.irrprice]);
-  useEffect(() => {
-    if (locale === "fa" && product?.irrdiscount) {
-      let formattedDiscount = new Intl.NumberFormat("fa-IR").format(
-        product?.irrdiscount
-      );
-      setIRRDiscount(formattedDiscount);
-    }
-  }, [locale, product?.irrdiscount]);
 
   return (
     <div className="w-full px-2 my-2">
       <div className="w-full p-3 shadow-lg backdrop-filter backdrop-blur-[10px] bg-palette-card/80 rounded-md">
-        {product?.image[0] && (
-          <Image
-            src={urlFor(product?.image[0]).url()}
-            alt="laptop image"
-            width={200}
-            height={185}
-            className="object-contain hover:scale-105 transition-transform"
-          />
-        )}
+        <div className="text-center">
+          {product?.image[0] && (
+            <Image
+              src={urlFor(product?.image[0]).url()}
+              alt="laptop image"
+              width={200}
+              height={185}
+              className="object-contain hover:scale-105 transition-transform !p-2"
+            />
+          )}
+          <span className="block absolute -top-2 -right-2">
+            <Image
+              src="/images/discount-icon/discount.png"
+              width={40}
+              height={40}
+              alt="discount-icon"
+            />
+          </span>
+        </div>
         <p className="truncate">{product?.name}</p>
         <span>
-          <del className="text-[10px] md:text-sm text-rose-600">
+          <del className="text-[12px] md:text-sm text-rose-600">
             <sup className="mr-1">{locale === "en" ? "£" : ""}</sup>
-            {product?.price
-              ? locale === "fa"
-                ? IRRPrice
-                : product?.price
-              : null}
+            {locale === "en"
+              ? gbpCurrencyFormat(product?.price)
+              : irrCurrencyFormat(product?.irrprice)}
             <sub className="ml-1">{locale === "fa" ? "تومان" : ""}</sub>
           </del>
-          {product?.discount && (
+          {product?.discount ? (
             <>
               <br />
-              <ins className="text-[12px] md:text-[16px] font-bold self-end mt-6 ">
+              <ins className="text-sm md:text-[16px] font-bold self-end mt-6 ">
                 <sup className="mr-1">{locale === "en" ? "£" : ""}</sup>
-                {product?.discount
-                  ? locale === "fa"
-                    ? IRRDiscount
-                    : product?.discount
-                  : null}
+                {locale === "en"
+                  ? gbpCurrencyFormat(product?.discount)
+                  : irrCurrencyFormat(product?.irrdiscount)}
                 <sub className="ml-1">{locale === "fa" ? "تومان" : ""}</sub>
               </ins>
             </>
-          )}
+          ) : null}
         </span>
       </div>
     </div>
