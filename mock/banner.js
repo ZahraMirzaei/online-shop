cd6db55b6ac8e76085938820ae871eff97bc0ae7
ZahraMirzaei.JS <z.mirzaei2019@gmail.com>
2022-06-19 20:38:55 +0430

    feat: :clown_face: add banners mock file

---
+++ mock/banner.js
@@ -0,0 +1,22 @@
+export const bannerContent = [
+  {
+    title: "larisaTitle",
+    description: "larisaDescription",
+    buttonText: "see",
+    imgSrc: "/images/banners-img/home1.png",
+    imgWidth: 980,
+    imgHeight: 500,
+    numberOfDiscountDate: 9,
+    href: "/",
+  },
+  {
+    title: "romanoTitle",
+    description: "romanoDescription",
+    buttonText: "see",
+    imgSrc: "/images/banners-img/home2.jpg",
+    imgWidth: 980,
+    imgHeight: 500,
+    numberOfDiscountDate: 7,
+    href: "/",
+  },
+];
