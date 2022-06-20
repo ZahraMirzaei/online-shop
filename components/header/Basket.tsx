c8585fc12fcf68852908eacfa7fbc2b475156979
ZahraMirzaei.JS <z.mirzaei2019@gmail.com>
2022-06-20 12:46:32 +0430

    feat: :sparkles: change basket icon to Link and modify some styles

--- components/header/Basket.tsx
+++ components/header/Basket.tsx
@@ -1,14 +1,17 @@
 import React from "react";
 import { AiOutlineShoppingCart } from "react-icons/ai";
+import Link from "next/link";
 
 const Basket = () => {
   return (
-    <div className="relative flex items-center">
-      <AiOutlineShoppingCart style={{ fontSize: "1.6rem" }} />
-      <span className="absolute -top-3 -right-[0.3rem] flex items-center justify-center w-5 h-5 rtl:pt-[0.1rem] rounded-full bg-palette-primary text-[0.75rem] leading-3 text-white shadow-lg">
-        0
-      </span>
-    </div>
+    <Link href="">
+      <a className="relative flex items-center ltr:md:pl-6 rtl:md:pr-6 md:border-r-2 md:border-r-slate-300">
+        <AiOutlineShoppingCart style={{ fontSize: "1.6rem" }} />
+        <span className="absolute -top-3 -right-[0.3rem] md:right-[1rem] flex items-center justify-center w-5 h-5 rtl:pt-[0.1rem] rounded-full bg-palette-primary text-[0.75rem] leading-3 text-white shadow-lg">
+          0
+        </span>
+      </a>
+    </Link>
   );
 };
 
