291b0d23b449716841270c6e67345961374af014
ZahraMirzaei.JS <z.mirzaei2019@gmail.com>
2022-06-20 11:01:56 +0430

    feat: :clown_face: add brand mock file

---
+++ mock/brand.js
@@ -0,0 +1,122 @@
+export const brandContent = [
+  {
+    id: 1,
+    name: "adidas",
+    imgSrc: "./images/brand-logo-img/adidas.png",
+  },
+  {
+    id: 2,
+    name: "apple",
+    imgSrc: "./images/brand-logo-img/apple.png",
+  },
+  {
+    id: 3,
+    name: "asus",
+    imgSrc: "./images/brand-logo-img/asus.png",
+  },
+  {
+    id: 4,
+    name: "benq",
+    imgSrc: "./images/brand-logo-img/benq.png",
+  },
+  {
+    id: 5,
+    name: "bvlgari",
+    imgSrc: "./images/brand-logo-img/bvlgari.png",
+  },
+  {
+    id: 6,
+    name: "del",
+    imgSrc: "./images/brand-logo-img/del.png",
+  },
+  {
+    id: 7,
+    name: "dior",
+    imgSrc: "./images/brand-logo-img/dior.png",
+  },
+  {
+    id: 8,
+    name: "dolce-gabbana",
+    imgSrc: "./images/brand-logo-img/dolce-gabbana.png",
+  },
+  {
+    id: 9,
+    name: "gucci",
+    imgSrc: "./images/brand-logo-img/gucci.png",
+  },
+  {
+    id: 10,
+    name: "hp",
+    imgSrc: "./images/brand-logo-img/hp.png",
+  },
+  {
+    id: 11,
+    name: "lg",
+    imgSrc: "./images/brand-logo-img/lg.png",
+  },
+  {
+    id: 12,
+    name: "loreal",
+    imgSrc: "./images/brand-logo-img/loreal.png",
+  },
+  {
+    id: 13,
+    name: "louis-vuitton",
+    imgSrc: "./images/brand-logo-img/louis-vuitton.png",
+  },
+  {
+    id: 14,
+    name: "mac",
+    imgSrc: "./images/brand-logo-img/mac.png",
+  },
+  {
+    id: 15,
+    name: "maybelline",
+    imgSrc: "./images/brand-logo-img/maybelline.png",
+  },
+  {
+    id: 16,
+    name: "msi",
+    imgSrc: "./images/brand-logo-img/msi.png",
+  },
+  {
+    id: 17,
+    name: "patek-philippe",
+    imgSrc: "./images/brand-logo-img/patek-philippe.png",
+  },
+  {
+    id: 18,
+    name: "puma",
+    imgSrc: "./images/brand-logo-img/puma.png",
+  },
+  {
+    id: 19,
+    name: "rolex",
+    imgSrc: "./images/brand-logo-img/rolex.png",
+  },
+  {
+    id: 20,
+    name: "samsung",
+    imgSrc: "./images/brand-logo-img/samsung.png",
+  },
+  {
+    id: 21,
+    name: "sony",
+    imgSrc: "./images/brand-logo-img/sony.png",
+  },
+  {
+    id: 22,
+    name: "toshiba",
+    imgSrc: "./images/brand-logo-img/toshiba.png",
+  },
+  {
+    id: 23,
+    name: "versace",
+    imgSrc: "./images/brand-logo-img/versace.png",
+  },
+  {
+    id: 24,
+    name: "xiaomi",
+    imgSrc: "./images/brand-logo-img/xiaomi.png",
+  },
+];
