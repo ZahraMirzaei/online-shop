833d86d1bb2c4061cff40293716bed12ef62b309
ZahraMirzaei.JS <z.mirzaei2019@gmail.com>
2022-06-21 10:39:08 +0430

    feat: :sparkles: add FooterColumns for all path links in footer

---
+++ components/footer/footerContent/FooterColumns.tsx
@@ -0,0 +1,34 @@
+import Link from "next/link";
+import React from "react";
+import { footerContent } from "../../../mock/footer";
+import { useLanguage } from "../../../hooks/useLanguage";
+
+const FooterColumns = () => {
+  const { t } = useLanguage();
+  return (
+    <div className="flex flex-wrap flex-grow min-width-[800px] xl:rtl:pl-60 justify-between">
+      {footerContent.map((item) => {
+        return (
+          <div className="mt-4 sm:mt-0" key={item.title}>
+            <h4 className="text-md rtl:border-r-4 ltr:border-l-4 border-palette-primary px-2">
+              {t[item.title]}
+            </h4>
+            <div className="flex flex-col mt-2">
+              {item.subtitles.map((subItem) => {
+                return (
+                  <Link href={subItem.href} key={subItem.text}>
+                    <a className="text-sm text-palette-base/70 px-4 py-2 hover:text-palette-base/100">
+                      {t[subItem.text]}
+                    </a>
+                  </Link>
+                );
+              })}
+            </div>
+          </div>
+        );
+      })}
+    </div>
+  );
+};
+
+export default FooterColumns;
