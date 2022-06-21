dc80ffb19a98f198e880279f912363a57ce88636
ZahraMirzaei.JS <z.mirzaei2019@gmail.com>
2022-06-21 10:51:21 +0430

    style: :lipstick: change BrandBox style

--- components/brands/BrandBox.tsx
+++ components/brands/BrandBox.tsx
@@ -7,7 +7,7 @@ interface Props {
 }
 const BrandBox: React.FC<Props> = ({ brandName, imageSrc }) => {
   return (
-    <div className="relative flex items-center lg:p-2">
+    <div className="relative flex items-center p-3 lg:p-2 shadow-md lg:shadow-xl">
       <Image src={imageSrc} width={300} height={175} alt={brandName} />
       <div className="absolute dark:inset-0 dark:bg-slate-800/40"></div>
     </div>
