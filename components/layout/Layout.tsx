90506899630c3c6888fbcbaf49292a922cff7d91
ZahraMirzaei.JS <z.mirzaei2019@gmail.com>
2022-06-21 10:48:42 +0430

    feat: :sparkles: import Footer to layout component and add more structure to layout
    As the header position is fixed, other contents must be moved downward, and footer should always be at end of the viewport.

--- components/layout/Layout.tsx
+++ components/layout/Layout.tsx
@@ -5,21 +5,26 @@ import { ThemeProvider } from "next-themes";
 import Header from "../header";
 import { SidebarContextProvider } from "../../store/context/NavContext";
 import store from "../../store/index";
+import Footer from "../footer";
 
 const Layout: React.FC<{ children?: React.ReactNode }> = ({ children }) => {
   return (
     <Provider store={store}>
       <ThemeProvider enableSystem={true} attribute="class">
         <Head>
-          <title>ZiShop e-commerce</title>
+          <title>ZiShop</title>
         </Head>
-        <header>
-          <SidebarContextProvider>
-            <Header />
-          </SidebarContextProvider>
-        </header>
-        <main>{children}</main>
-        <footer>footer</footer>
+        <div className="flex flex-col min-h-[100vh]">
+          <header>
+            <SidebarContextProvider>
+              <Header />
+            </SidebarContextProvider>
+          </header>
+          <main className="flex-grow  md:mt-40">{children}</main>
+          <footer>
+            <Footer />
+          </footer>
+        </div>
       </ThemeProvider>
     </Provider>
   );
