8978ef3e5b583690eed512a19c502c30f827bbe4
ZahraMirzaei.JS <z.mirzaei2019@gmail.com>
2022-06-22 21:27:43 +0430

    feat: :sparkles: add megamenuContext to MenuItems and add href to Link tag base on break point width

--- components/UI/MenuItems.tsx
+++ components/UI/MenuItems.tsx
@@ -5,33 +5,46 @@ import menuItems from "../../mock/menuItems";
 import { HiChevronRight, HiChevronLeft } from "react-icons/hi";
 import { IDropDown } from "../../lib/types/dropDown";
 import ActiveMenuItemContext from "../../store/context/activeMenuItemContext";
+import { useRouter } from "next/router";
+import { useWindowDimensions } from "../../hooks/useWindowDimensions";
+import MegaMenuContext from "../../store/context/megaMenuContext";
 
 interface Props {
-  onClick?: (submenu: IDropDown[] | undefined) => void;
-  onMouseOver?: (submenu: IDropDown[] | undefined, index: number) => void;
+  onClick?: (submenu: IDropDown[] | undefined, activeItemName: string) => void;
+  onMouseOver?: (
+    submenu: IDropDown[] | undefined,
+    index: number,
+    activeItemName: string
+  ) => void;
 }
 const MenuItems: React.FC<Props> = (props) => {
   const { t, locale } = useLanguage();
+  const route = useRouter();
+  const { width } = useWindowDimensions();
   const ArrowDirection = locale === "en" ? HiChevronRight : HiChevronLeft;
   const activeItemCtx = useContext(ActiveMenuItemContext);
+  const megaMenuCtx = useContext(MegaMenuContext);
   return (
     <ul className="rounded-lg">
       {menuItems.map((item, index) => {
         return (
           <li
-            className="py-3 md:py-3 transition-all hover:text-palette-primary font-bold"
+            className="py-3 md:py-3 transition-color duration-300 hover:text-palette-primary font-bold"
             key={item.category}
           >
-            <Link href="/">
+            <Link href={width >= 768 ? `/${item.category}` : route.asPath}>
               <a
                 className={`flex items-center mt-3 px-5  cursor-pointer text-sm ${
                   index === activeItemCtx.activeMenuItemIndex
                     ? "md:text-palette-primary"
                     : ""
                 }`}
-                onClick={() => props.onClick?.(item.productsGroup)}
+                onClick={() => {
+                  props.onClick?.(item.productsGroup, item.category);
+                  width >= 768 && megaMenuCtx.closeMegaMenu();
+                }}
                 onMouseOver={() =>
-                  props.onMouseOver?.(item.productsGroup, index)
+                  props.onMouseOver?.(item.productsGroup, index, item.category)
                 }
               >
                 <item.icon className="w-6 h-6 " />
