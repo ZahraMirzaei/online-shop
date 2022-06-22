f9a96a11c09acc269ec63e52e525f53521443610
ZahraMirzaei.JS <z.mirzaei2019@gmail.com>
2022-06-22 18:21:55 +0430

    style: :lipstick: add ltr style to basket icon

--- components/header/Basket.tsx
+++ components/header/Basket.tsx
@@ -4,10 +4,10 @@ import Link from "next/link";
 
 const Basket = () => {
   return (
-    <Link href="">
+    <Link href="/">
       <a className="relative flex items-center ltr:md:pl-6 rtl:md:pr-6 rtl:md:border-r-2 rtl:md:border-r-slate-300 ltr:md:border-l-2 ltr:md:border-l-slate-300">
         <AiOutlineShoppingCart style={{ fontSize: "1.6rem" }} />
-        <span className="absolute -top-3 -right-[0.3rem] md:right-[1rem] flex items-center justify-center w-5 h-5 rtl:pt-[0.1rem] rounded-full bg-palette-primary text-[0.75rem] leading-3 text-white shadow-lg">
+        <span className="absolute -top-3 rtl:-right-[0.3rem] rtl:md:right-[1rem] ltr:-left-[0.3rem] ltr:md:left-[1rem] flex items-center justify-center w-5 h-5 rtl:pt-[0.1rem] rounded-full bg-palette-primary text-[0.75rem] leading-3 text-white shadow-lg">
           0
         </span>
       </a>
