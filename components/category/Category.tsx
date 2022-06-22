1335db3b3da4ccc29f1fcf7a03790d6dfea57354
ZahraMirzaei.JS <z.mirzaei2019@gmail.com>
2022-06-22 18:20:16 +0430

    feat: :sparkles: add link href and hover effect to category box

--- components/category/Category.tsx
+++ components/category/Category.tsx
@@ -30,12 +30,12 @@ const Category = () => {
 
       {/* 💻lg break point */}
       <div className="hidden lg:grid  gap-4 grid-rows-9 grid-cols-2 md:grid-cols-9 max-w-[1700px] mx-auto">
-        <div className="flex justify-around items-center md:row-span-6 md:col-span-3 bg-palette-digitalCategory py-8 px-4  rounded-md shadow-lg overflow-hidden">
+        <div className="flex justify-around items-center md:row-span-6 md:col-span-3 bg-palette-digitalCategory py-8 px-4 rounded-md shadow-lg overflow-hidden">
           <div>
             <h3 className="text-2xl font-[500]">{t.digitalCategoryTitle}</h3>
             <p className="mt-4">{t.digitalCategoryDescription}</p>
-            <Link href="/">
-              <a className="inline-block shadow-lg border-none bg-palette-primary text-palette-side py-3 px-4 rounded-lg mt-4 text-sm">
+            <Link href="/digital">
+              <a className="inline-block shadow-lg border-none bg-palette-primary/80 hover:bg-palette-primary/100 hover:shadow-md transition-all duration-300 text-palette-side py-3 px-4 rounded-lg mt-4 text-sm">
                 {t.seeAllProducts}
               </a>
             </Link>
@@ -46,15 +46,15 @@ const Category = () => {
             alt="digital"
             width={190}
             height={240}
-            className="drop-shadow-lg"
+            className="drop-shadow-lg hover:scale-95 transition-transform duration-300"
           />
         </div>
         <div className="flex justify-around items-center md:row-span-6 md:col-span-3 bg-palette-fashionCategory  rounded-md shadow-lg px-4 ">
           <div>
             <h3 className="text-xl font-[500]">{t.fashionCategoryTitle}</h3>
             <p className="text-[12px] mt-4">{t.fashionCategoryDescription}</p>
-            <Link href="/">
-              <a className="inline-block shadow-lg border-none bg-palette-primary text-palette-side py-3 px-2 xl:px-4 rounded-lg mt-4 text-[12px] xl:text-sm">
+            <Link href="/fashion">
+              <a className="inline-block shadow-lg border-none bg-palette-primary text-palette-side py-3 px-2 xl:px-4 rounded-lg mt-4 text-[12px] xl:text-sm bg-palette-primary/80 hover:bg-palette-primary/100 hover:shadow-md transition-all duration-300">
                 {t.seeAllProducts}
               </a>
             </Link>
@@ -65,15 +65,15 @@ const Category = () => {
             alt="fashion"
             width={240}
             height={250}
-            className="drop-shadow-lg self-start"
+            className="drop-shadow-lg self-start  hover:scale-95 transition-transform duration-300"
           />
         </div>
         <div className="flex justify-around items-center md:row-span-3 md:col-span-3 rounded-md shadow-lg bg-palette-beautyCategory px-4 py-2">
           <div>
             <h3 className="text-lg font-[500]">{t.beautyCategoryTitle}</h3>
             <p className="text-[12px] mt-4">{t.beautyCategoryDescription}</p>
-            <Link href="/">
-              <a className="inline-block shadow-lg border-none bg-palette-primary text-palette-side py-3 px-4 rounded-lg mt-4 text-sm">
+            <Link href="/beauty">
+              <a className="inline-block shadow-lg border-none bg-palette-primary text-palette-side py-3 px-4 rounded-lg mt-4 text-sm bg-palette-primary/80 hover:bg-palette-primary/100 hover:shadow-md transition-all duration-300">
                 {t.seeAllProducts}
               </a>
             </Link>
@@ -84,7 +84,7 @@ const Category = () => {
             alt="beauty"
             width={170}
             height={150}
-            className="drop-shadow-lg self-start"
+            className="drop-shadow-lg self-start  hover:scale-95 transition-transform duration-300"
           />
         </div>
         <div className="flex justify-around items-center md:row-span-3 md:col-span-3 rounded-md shadow-lg bg-palette-sportCategory">
@@ -93,13 +93,13 @@ const Category = () => {
             alt="sport"
             width={130}
             height={150}
-            className="drop-shadow-lg self-start"
+            className="drop-shadow-lg self-start  hover:scale-95 transition-transform duration-300"
           />
           <div>
             <h3 className="text-lg font-[500]">{t.sportCategoryTitle}</h3>
             <p className="text-[12px] mt-4">{t.sportCategoryDescription}</p>
-            <Link href="/">
-              <a className="inline-block shadow-lg border-none bg-palette-primary text-palette-side py-3 px-4 rounded-lg mt-4 text-sm">
+            <Link href="/sport">
+              <a className="inline-block shadow-lg border-none bg-palette-primary text-palette-side py-3 px-4 rounded-lg mt-4 text-sm bg-palette-primary/80 hover:bg-palette-primary/100 hover:shadow-md transition-all duration-300">
                 {t.seeAllProducts}
               </a>
             </Link>
@@ -109,8 +109,8 @@ const Category = () => {
           <div className="">
             <h3 className="text-2xl font-[500]">{t.houseCategoryTitle}</h3>
             <p className="text-md mt-4">{t.houseCategoryDescription}</p>
-            <Link href="/">
-              <a className="inline-block shadow-lg border-none bg-palette-primary text-palette-side py-3 px-4 rounded-lg mt-4 text-sm">
+            <Link href="/house">
+              <a className="inline-block shadow-lg border-none bg-palette-primary text-palette-side py-3 px-4 rounded-lg mt-4 text-sm bg-palette-primary/80 hover:bg-palette-primary/100 hover:shadow-md transition-all duration-300">
                 {t.seeAllProducts}
               </a>
             </Link>
@@ -120,15 +120,15 @@ const Category = () => {
             alt="house"
             width={320}
             height={240}
-            className="drop-shadow-lg self-start"
+            className="drop-shadow-lg self-start  hover:scale-95 transition-transform duration-300"
           />
         </div>
         <div className="flex flex-col justify-around items-center md:row-span-2 md:col-span-2 rounded-md shadow-lg bg-palette-toyCategory">
           <div className="text-center">
             <h3 className="text-lg font-[500]">{t.toyCategoryTitle}</h3>
             <p className="text-[12px] mt-3">{t.toyCategoryDescription}</p>
-            <Link href="/">
-              <a className="inline-block shadow-lg border-none bg-palette-primary text-palette-side py-3 px-4 rounded-lg mt-3 text-sm">
+            <Link href="/toy">
+              <a className="inline-block shadow-lg border-none bg-palette-primary text-palette-side py-3 px-4 rounded-lg mt-3 text-sm bg-palette-primary/80 hover:bg-palette-primary/100 hover:shadow-md transition-all duration-300">
                 {t.seeAllProducts}
               </a>
             </Link>
@@ -138,7 +138,7 @@ const Category = () => {
             alt="toy"
             width={130}
             height={110}
-            className="drop-shadow-lg self-start"
+            className="drop-shadow-lg self-start  hover:scale-95 transition-transform duration-300"
           />
         </div>
         <div className="flex justify-around items-center md:row-span-2 md:col-span-2 rounded-md shadow-lg px-3 bg-palette-stationeryCategory">
@@ -147,8 +147,8 @@ const Category = () => {
             <p className="text-[12px] mt-3">
               {t.stationeryCategoryDescription}
             </p>
-            <Link href="/">
-              <a className="inline-block shadow-lg border-none bg-palette-primary text-palette-side py-3 px-2 rounded-lg mt-3 text-[10px] xl:text-[12px]">
+            <Link href="/stationery">
+              <a className="inline-block shadow-lg border-none bg-palette-primary text-palette-side py-3 px-2 rounded-lg mt-3 text-[10px] xl:text-[12px] bg-palette-primary/80 hover:bg-palette-primary/100 hover:shadow-md transition-all duration-300">
                 {t.seeAllProducts}
               </a>
             </Link>
@@ -158,7 +158,7 @@ const Category = () => {
             alt="stationery"
             width={130}
             height={250}
-            className="drop-shadow-lg self-start"
+            className="drop-shadow-lg self-start  hover:scale-95 transition-transform duration-300"
           />
         </div>
       </div>
