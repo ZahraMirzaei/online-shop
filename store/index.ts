e5c1143ad5196a293e4226972b983d94f11803fd
ZahraMirzaei.JS <z.mirzaei2019@gmail.com>
2022-06-23 12:57:14 +0430

    feat: :sparkles: add sortedProductsList to store in redux

--- store/index.ts
+++ store/index.ts
@@ -2,11 +2,13 @@ import { configureStore } from "@reduxjs/toolkit";
 
 import specialOfferProductsReducer from "./specialOfferProducts-slice";
 import newestProductReducer from "./newestProduct-slice";
+import SortedProductsListReducer from "./sortedProductList-slice";
 
 const store = configureStore({
   reducer: {
     specialOfferProductsList: specialOfferProductsReducer,
     newestProductsList: newestProductReducer,
+    sortedProductsList: SortedProductsListReducer,
   },
 });
 
