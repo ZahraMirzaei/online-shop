1356166a5db7f7ca465d6116009489b58493152b
ZahraMirzaei.JS <z.mirzaei2019@gmail.com>
2022-06-23 12:05:01 +0430

    feat: :sparkles: add Sort component to ProductList component

--- components/productList/ProductList.tsx
+++ components/productList/ProductList.tsx
@@ -1,9 +1,10 @@
 import React from "react";
+import { useLanguage } from "../../hooks/useLanguage";
 import { IProduct } from "../../lib/types/products";
 import SubmenuCategory from "./SubmenuCategory";
 import Card from "../UI/card/Card";
 import Breadcrumb from "../UI/Breadcrumb";
-import { useLanguage } from "../../hooks/useLanguage";
+import Sort from "./Sort";
 
 interface Props {
   productList: IProduct[];
@@ -15,18 +16,21 @@ const ProductList: React.FC<Props> = ({ productList }) => {
       <Breadcrumb />
       <SubmenuCategory />
       {productList?.length ? (
-        <div className="grid gap-4 md:gap-2 grid-cols-6 md:grid-cols-12 max-w-[1700px]">
-          {productList
-            ? productList.map((product) => {
-                return (
-                  <Card
-                    key={product.slug.current}
-                    product={product}
-                    href={`/`}
-                  />
-                );
-              })
-            : null}
+        <div>
+          <Sort />
+          <div className="grid gap-4 md:gap-2 grid-cols-6 md:grid-cols-12 max-w-[1700px]">
+            {productList
+              ? productList.map((product) => {
+                  return (
+                    <Card
+                      key={product.slug.current}
+                      product={product}
+                      href={`/`}
+                    />
+                  );
+                })
+              : null}
+          </div>
         </div>
       ) : (
         <p className="text-palette-mute text-center mt-8">{t.noProduct}</p>
