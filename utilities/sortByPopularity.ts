caa19f2969d9ade868dd1dfa3a4878a82b2a8179
ZahraMirzaei.JS <z.mirzaei2019@gmail.com>
2022-06-23 15:40:31 +0430

    fix: :bug: fix sortByPopularity function

--- utilities/sortByPopularity.ts
+++ utilities/sortByPopularity.ts
@@ -4,5 +4,5 @@ export const sortByPoPularity = (
   product1: IProduct,
   product2: IProduct
 ): number => {
-  return product1.starRating - product2.starRating;
+  return product2.starRating - product1.starRating;
 };
