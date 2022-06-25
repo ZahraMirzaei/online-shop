7f93f3b2c82b9e76e47fb72ec907e5979e4672a3
ZahraMirzaei.JS <z.mirzaei2019@gmail.com>
2022-06-25 21:59:18 +0430

    feat: :sparkles: add newestProduct page

---
+++ pages/newestProduct.tsx
@@ -0,0 +1,36 @@
+import type { NextPage } from "next";
+import { useState, useEffect } from "react";
+import { GetStaticProps } from "next";
+import { client } from "../lib/client";
+import { IProduct } from "../lib/types/products";
+import ProductList from "../components/productList/ProductList";
+import { newestProductsFn } from "../utilities/sortByTimeStamp";
+
+const NewestProduct: NextPage<{
+  products: IProduct[];
+}> = ({ products }) => {
+  const [productsList, setProductsList] = useState<IProduct[] | []>([]);
+
+  useEffect(() => {
+    setProductsList(newestProductsFn(products));
+  }, [products]);
+
+  return (
+    <div className="flex flex-wrap">
+      {productsList.length ? <ProductList productList={productsList} /> : null}
+    </div>
+  );
+};
+
+export default NewestProduct;
+
+export const getStaticProps: GetStaticProps = async () => {
+  const productQuery = `*[_type=='product' && slug.current != "asus-zenbook-14-intel-core-i7-16gb-ram-512gb-ssd-14-ips-laptop"]`;
+  const products = await client.fetch(productQuery);
+  console.log(products);
+  return {
+    props: {
+      products: products,
+    },
+  };
+};
