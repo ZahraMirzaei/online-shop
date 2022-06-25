9be5800cc24ad7e6e9dfb0d8dcb0714bb8bd145c
ZahraMirzaei.JS <z.mirzaei2019@gmail.com>
2022-06-25 19:55:56 +0430

    feat: :sparkles: add Benefit and SimilarProducts components to ProdictDetails component and add responsive styles

--- components/productDetails/index.tsx
+++ components/productDetails/index.tsx
@@ -3,6 +3,8 @@ import { IProduct } from "../../lib/types/products";
 import Breadcrumb from "../UI/Breadcrumb";
 import ImageSection from "./ImageSection";
 import DetailsSection from "./DetailsSection";
+import Benefits from "../Benefits";
+import SimilarProducts from "./SimilarProducts";
 
 interface Props {
   product: IProduct;
@@ -10,12 +12,16 @@ interface Props {
 }
 const ProductDetails: React.FC<Props> = ({ product, products }) => {
   return (
-    <div className="flex flex-col">
+    <div className="flex flex-col max-w-[1500px] mx-auto">
       <Breadcrumb />
-      <div className="flex flex-col md:flex-row flex-wrap items-center">
+      <div className="flex flex-col md:flex-row flex-wrap md:flex-nowrap items-center md:items-start mt-8 relative">
         <ImageSection imgArray={product.image} />
         <DetailsSection product={product} />
       </div>
+      <div className="border-2 my-8">
+        <Benefits />
+      </div>
+      <SimilarProducts products={products} />
     </div>
   );
 };
