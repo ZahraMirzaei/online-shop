32b39fa1007054a59c30179f84314bffea94d988
ZahraMirzaei.JS <z.mirzaei2019@gmail.com>
2022-06-25 19:45:36 +0430

    style: :lipstick: add z-index to ProductPageAction

--- components/productDetails/ProductPageActions.tsx
+++ components/productDetails/ProductPageActions.tsx
@@ -1,11 +1,11 @@
 import React from "react";
-import { AiOutlineHeart, AiOutlineShoppingCart } from "react-icons/ai";
+import { AiOutlineHeart } from "react-icons/ai";
 import { IoShareSocialOutline } from "react-icons/io5";
 
 const ProductPageActions = () => {
   return (
-    <div className=" py-4 -mt-6 flex flex-col justify-evenly absolute top-0 ltr:left-0 rtl:right-0 md:static rounded-lg">
-      <div className="hover:text-rose-600 transition-colors px-2 md:px-6 py-3">
+    <div className=" py-4 -mt-6 flex flex-col justify-evenly absolute top-0 ltr:left-0 rtl:right-0 md:static rounded-lg z-10">
+      <div className="hover:text-rose-600 transition-colors px-2 md:px-6 py-3 ">
         <AiOutlineHeart style={{ fontSize: "1.5rem" }} />
       </div>
       <div className="hover:text-rose-600 transition-colors px-2 md:px-6 py-3">
