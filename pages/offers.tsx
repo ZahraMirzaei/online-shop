7cf95c0f45d411bcb41a41cdd9b5bb9fa9bd1a78
ZahraMirzaei.JS <z.mirzaei2019@gmail.com>
2022-06-25 20:53:22 +0430

    feat: :sparkles: create offers page
    for show all product that have discount

---
+++ pages/offers.tsx
@@ -0,0 +1,27 @@
+import type { NextPage } from "next";
+import { GetStaticProps, GetStaticPaths } from "next";
+import { client } from "../lib/client";
+import { IProduct } from "../lib/types/products";
+import ProductList from "../components/productList/ProductList";
+
+const offers: NextPage<{
+  products: IProduct[];
+}> = ({ products }) => {
+  return (
+    <div>
+      <ProductList productList={products} />
+    </div>
+  );
+};
+
+export default offers;
+
+export const getStaticProps: GetStaticProps = async () => {
+  const productQuery = `*[_type=='product'&& isOffer==true]`;
+  const products = await client.fetch(productQuery);
+  return {
+    props: {
+      products: products,
+    },
+  };
+};
