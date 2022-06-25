1e0499cd17f6d8b954b9855120164007b5aba6d9
ZahraMirzaei.JS <z.mirzaei2019@gmail.com>
2022-06-25 17:56:29 +0430

    style: :lipstick: change Breadcrumb style to don't wrap in small size of device
    make it overflow:auto to show scrollbar

--- components/UI/Breadcrumb.tsx
+++ components/UI/Breadcrumb.tsx
@@ -37,9 +37,9 @@ const Breadcrumb = () => {
   }
 
   return (
-    <div className="flex text-[12px] sm:text-sm text-palette-mute mt-4 md:-mt-4 mb-3 md:my-none">
+    <div className="flex text-[11px] sm:text-sm text-palette-mute mt-4 md:-mt-4 mb-3 md:my-none overflow-auto whitespace-nowrap">
       <nav className="flex py-3 px-2 sm:px-5 leading-6">
-        <ul className="flex items-center flex-wrap space-x-1 md:space-x-3">
+        <ul className="flex items-center space-x-1 md:space-x-3">
           <li className="cursor-pointer">
             <Link href="/">
               <a className="inline-block ltr:pr-2 rtl:pl-2">{t.mainPage}</a>
