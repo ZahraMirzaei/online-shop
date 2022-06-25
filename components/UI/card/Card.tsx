57028fb4790503ef7224d17969bffb2788bbef62
ZahraMirzaei.JS <z.mirzaei2019@gmail.com>
2022-06-25 20:08:26 +0430

    feat: :sparkles: add wrapper around Image on Card component to move it to center in very small devices
    for responsive it perfectly

--- components/UI/card/Card.tsx
+++ components/UI/card/Card.tsx
@@ -25,13 +25,15 @@ const Card: React.FC<Props> = ({ href, product }) => {
       >
         <a className="flex md:items-center md:flex-col relative w-full">
           <div className="w-full relative bg-slate-400/30 px-1 md:px-6 py-2 rounded-bl-xl rounded-tl-xl md:rounded-tr-xl md:rounded-bl-none rtl:order-2 rtl:md:order-none flex flex-col justify-between items-center">
-            <Image
-              src={urlFor(product?.image[0]).url()}
-              width={280}
-              height={300}
-              alt={product.name}
-              className=" drop-shadow-xl object-contain hover:scale-110 transition-transform duration-300 ease-in-out !py-2"
-            />
+            <div className="flex items-center h-full">
+              <Image
+                src={urlFor(product?.image[0]).url()}
+                width={280}
+                height={300}
+                alt={product.name}
+                className=" drop-shadow-xl object-contain hover:scale-110 transition-transform duration-300 ease-in-out !py-2 "
+              />
+            </div>
             {product?.discount ? (
               <span className="w-8 sm:w-auto block absolute -top-2 -right-2">
                 <Image
