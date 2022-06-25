baa92abbdf77d1ba73922949b14a6bfc6b192b0c
ZahraMirzaei.JS <z.mirzaei2019@gmail.com>
2022-06-25 17:35:05 +0430

    refactor: :recycle: make CarouselBox reusable
    move Slider component from Offers component to CarouselBox component - add `full` props to CarouselBox in case of use CarouselBox in ProductDetailsPage

--- components/Offers/Offers.tsx
+++ components/Offers/Offers.tsx
@@ -1,68 +1,20 @@
 import React from "react";
 import CarouselBox from "../UI/CarouselBox/CarouselBox";
 import { useSelector } from "react-redux";
-import Slider from "react-slick";
 import { IProduct } from "../../lib/types/products";
 import CarouselBoxCard from "../UI/CarouselBox/CarouselBoxCard";
-import { NextArrow, PrevArrow } from "../UI/CarouselBox/CarouselBoxArrows";
 
 const Offers = () => {
   const OfferProducts = useSelector(
     (state: any) => state.specialOfferProductsList.specialOfferProducts
   );
 
-  const settings = {
-    className: "center",
-    infinite: false,
-    speed: 600,
-    centerPadding: "60px",
-    slidesToShow: 5,
-    slidesToScroll: 5,
-    // initialSlide: 0,
-    swipeToSlide: true,
-    // rtl: true,
-    nextArrow: <NextArrow />,
-    prevArrow: <PrevArrow />,
-    responsive: [
-      {
-        breakpoint: 1324,
-        settings: {
-          slidesToShow: 4,
-          slidesToScroll: 4,
-        },
-      },
-      {
-        breakpoint: 1024,
-        settings: {
-          slidesToShow: 3,
-          slidesToScroll: 3,
-        },
-      },
-      {
-        breakpoint: 768,
-        settings: {
-          slidesToShow: 2,
-          slidesToScroll: 2,
-        },
-      },
-      {
-        breakpoint: 640,
-        settings: {
-          slidesToShow: 1,
-          slidesToScroll: 1,
-        },
-      },
-    ],
-  };
-
   return (
     <div className="md:mt-10">
       <CarouselBox title="offers" className="bg-offersBG" href="/">
-        <Slider {...settings}>
-          {OfferProducts.slice(0, 10).map((product: IProduct) => {
-            return <CarouselBoxCard key={product.name} product={product} />;
-          })}
-        </Slider>
+        {OfferProducts.slice(0, 10).map((product: IProduct) => {
+          return <CarouselBoxCard key={product.name} product={product} />;
+        })}
       </CarouselBox>
     </div>
   );
--- components/UI/CarouselBox/CarouselBox.tsx
+++ components/UI/CarouselBox/CarouselBox.tsx
@@ -1,31 +1,100 @@
 import React from "react";
 import Link from "next/link";
 import { useLanguage } from "../../../hooks/useLanguage";
+import { NextArrow, PrevArrow } from "./CarouselBoxArrows";
+import Slider from "react-slick";
 
 interface Props {
   title: string;
-  className: string;
-  href: string;
+  className?: string;
+  href?: string;
   children?: React.ReactNode;
+  full?: boolean;
 }
-const CarouselBox: React.FC<Props> = ({ title, className, children, href }) => {
+const CarouselBox: React.FC<Props> = ({
+  title,
+  className,
+  children,
+  href,
+  full,
+}) => {
   const { t } = useLanguage();
 
+  const settings = {
+    className: ` px-4 ${full ? "bg-palette-fill" : "bg-[#37bccef9]"}`,
+    infinite: false,
+    speed: 600,
+    centerPadding: "60px",
+    slidesToShow: 5,
+    slidesToScroll: 5,
+    // initialSlide: 0,
+    swipeToSlide: true,
+    // rtl: true,
+    nextArrow: <NextArrow />,
+    prevArrow: <PrevArrow />,
+    responsive: [
+      {
+        breakpoint: 1324,
+        settings: {
+          slidesToShow: 4,
+          slidesToScroll: 4,
+        },
+      },
+      {
+        breakpoint: 1024,
+        settings: {
+          slidesToShow: 3,
+          slidesToScroll: 3,
+        },
+      },
+      {
+        breakpoint: 768,
+        settings: {
+          slidesToShow: 2,
+          slidesToScroll: 2,
+        },
+      },
+      {
+        breakpoint: 640,
+        settings: {
+          slidesToShow: 1,
+          slidesToScroll: 1,
+        },
+      },
+    ],
+  };
+
   return (
-    <div className="w-[85%] mx-auto flex bg-[#37bccef9] rounded-md">
+    <div
+      className={`w-[100%] mx-auto my-8 flex rounded-md ${
+        full ? "flex-col" : "bg-[#37bccef9]"
+      }`}
+    >
       <div
         className={`flex flex-col items-center justify-around flex-grow text-sm sm:text-base  bg-cover bg-no-repeat bg-center rounded-md backdrop-blur-md ${className}`}
       >
-        <h2 className="text-lg text-center sm:text-xl font-bold text-palette-primary">
+        <h2
+          className={`text-lg  sm:text-xl font-bold ${
+            full
+              ? "text-palette-base self-start"
+              : "text-palette-primary text-center"
+          } `}
+        >
           {t[`${title}`]}
         </h2>
-        <Link href={`${href}`}>
-          <a className="text-palette-primary/60 text-sm font-bold py-1 px-4 -mb-2 shadow-lg block rounded-full backdrop-filter backdrop-blur-[10px] bg-gray-300/10">
-            {t.seeAll}
-          </a>
-        </Link>
+        {!full ? (
+          <Link href={`${href}`}>
+            <a className="text-palette-primary/60 text-sm font-bold py-1 px-4 -mb-2 shadow-lg block rounded-full backdrop-filter backdrop-blur-[10px] bg-gray-300/10">
+              {t.seeAll}
+            </a>
+          </Link>
+        ) : null}
       </div>
-      <div className="w-[55%] sm:w-[75%] md:w-[85%]">{children}</div>
+      <div
+        className={`${full ? "w-full mt-4" : "w-[55%] sm:w-[75%] md:w-[85%]"}`}
+      >
+        <Slider {...settings}>{children}</Slider>
+      </div>
     </div>
   );
 };
