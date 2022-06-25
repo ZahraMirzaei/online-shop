f8b0d88240cdb01bf333af3ee164b697f0177e5d
ZahraMirzaei.JS <z.mirzaei2019@gmail.com>
2022-06-25 17:57:29 +0430

    refactor: :recycle: make CarouselBoxCard as Link to ProductDetailsPage

--- components/UI/CarouselBox/CarouselBoxCard.tsx
+++ components/UI/CarouselBox/CarouselBoxCard.tsx
@@ -7,6 +7,7 @@ import {
   gbpCurrencyFormat,
   irrCurrencyFormat,
 } from "../../../utilities/currencyFormat";
+import Link from "next/link";
 
 interface Props {
   product: IProduct;
@@ -17,49 +18,64 @@ const CarouselBoxCard: React.FC<Props> = ({ product }) => {
 
   return (
     <div className="w-full px-2 my-2">
-      <div className="w-full p-3 shadow-lg backdrop-filter backdrop-blur-[10px] bg-palette-card/80 rounded-md">
-        <div className="text-center">
-          {product?.image[0] && (
-            <Image
-              src={urlFor(product?.image[0]).url()}
-              alt="laptop image"
-              width={200}
-              height={185}
-              className="object-contain hover:scale-105 transition-transform !p-2"
-            />
-          )}
-          <span className="block absolute -top-2 -right-2">
-            <Image
-              src="/images/discount-icon/discount.png"
-              width={40}
-              height={40}
-              alt="discount-icon"
-            />
-          </span>
-        </div>
-        <p className="truncate">{product?.name}</p>
-        <span>
-          <del className="text-[12px] md:text-sm text-rose-600">
-            <sup className="mr-1">{locale === "en" ? "£" : ""}</sup>
-            {locale === "en"
-              ? gbpCurrencyFormat(product?.price)
-              : irrCurrencyFormat(product?.irrprice)}
-            <sub className="ml-1">{locale === "fa" ? "تومان" : ""}</sub>
-          </del>
-          {product?.discount ? (
-            <>
-              <br />
-              <ins className="text-sm md:text-[16px] font-bold self-end mt-6 ">
+      <Link
+        href={`/${product.category[0]}/${product.category[1]}/${product.brand}/${product.slug.current}`}
+      >
+        <a className="flex flex-col w-full p-3 shadow-lg backdrop-filter backdrop-blur-[10px] bg-palette-card/80 rounded-md">
+          <div className="text-center flex-grow">
+            {product?.image[0] && (
+              <Image
+                src={urlFor(product?.image[0]).url()}
+                alt="laptop image"
+                width={200}
+                height={185}
+                className="object-contain hover:scale-105 transition-transform !p-2"
+              />
+            )}
+            {product.isOffer ? (
+              <span className="block absolute -top-2 -right-2">
+                <Image
+                  src="/images/discount-icon/discount.png"
+                  width={40}
+                  height={40}
+                  alt="discount-icon"
+                />
+              </span>
+            ) : null}
+          </div>
+          <p className="truncate">{product?.name}</p>
+          <div className="w-full ltr:self-start rtl:self-end mt-4 text-left">
+            {product?.discount ? (
+              <span className="flex flex-col">
+                <del className="text-[12px] md:text-sm text-rose-600 ">
+                  <sup className="mr-1">{locale === "en" ? "£" : ""}</sup>
+                  {locale === "en"
+                    ? gbpCurrencyFormat(product?.price)
+                    : irrCurrencyFormat(product?.irrprice)}
+
+                  <sub className="ml-1">{locale === "fa" ? "تومان" : ""}</sub>
+                </del>
+                <ins className="text-base md:text-[16px] font-bold">
+                  <sup className="mr-1">{locale === "en" ? "£" : ""}</sup>
+                  {locale === "en"
+                    ? gbpCurrencyFormat(product?.discount)
+                    : irrCurrencyFormat(product?.irrdiscount)}
+                  <sub className="ml-1">{locale === "fa" ? "تومان" : ""}</sub>
+                </ins>
+              </span>
+            ) : (
+              <span className="font-bold underline">
+                <span className="block h-5"></span>
                 <sup className="mr-1">{locale === "en" ? "£" : ""}</sup>
                 {locale === "en"
-                  ? gbpCurrencyFormat(product?.discount)
-                  : irrCurrencyFormat(product?.irrdiscount)}
+                  ? gbpCurrencyFormat(product?.price)
+                  : irrCurrencyFormat(product?.irrprice)}
                 <sub className="ml-1">{locale === "fa" ? "تومان" : ""}</sub>
-              </ins>
-            </>
-          ) : null}
-        </span>
-      </div>
+              </span>
+            )}
+          </div>
+        </a>
+      </Link>
     </div>
   );
 };
