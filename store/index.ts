dee41f498383a019ce9e5be8adbc41299f036893
ZahraMirzaei.JS <z.mirzaei2019@gmail.com>
2022-06-26 10:10:32 +0430

    feat: :sparkles: add cartUi slice to store

--- store/index.ts
+++ store/index.ts
@@ -3,12 +3,14 @@ import { configureStore } from "@reduxjs/toolkit";
 import specialOfferProductsReducer from "./specialOfferProducts-slice";
 import newestProductReducer from "./newestProduct-slice";
 import SortedProductsListReducer from "./sortedProductList-slice";
+import cartUiReducer from "./cartUI-slice";
 
 const store = configureStore({
   reducer: {
     specialOfferProductsList: specialOfferProductsReducer,
     newestProductsList: newestProductReducer,
     sortedProductsList: SortedProductsListReducer,
+    cartUi: cartUiReducer,
   },
 });
 
