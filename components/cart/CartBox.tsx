b6b3015c371178d7e7a38a12608b96f9bf4a435f
ZahraMirzaei.JS <z.mirzaei2019@gmail.com>
2022-06-26 18:52:30 +0430

    feat: :sparkles: add header to CartBox component

--- components/cart/CartBox.tsx
+++ components/cart/CartBox.tsx
@@ -1,9 +1,29 @@
+import Link from "next/link";
 import React from "react";
+import { useDispatch, useSelector } from "react-redux";
+import { useLanguage } from "../../hooks/useLanguage";
+import { ICartRootState } from "../../lib/types/cart";
+import { changeNumbersFormatEnToFa } from "../../utilities/changeNumbersFormatEnToFa";
 
 const CartBox = () => {
+  const { t } = useLanguage();
+  const dispatch = useDispatch();
+  const cartItemQuantity = useSelector(
+    (state: ICartRootState) => state.cart.totalQuantity
+  );
+  const cartItems = useSelector((state: ICartRootState) => {
+    state.cart.items;
+  });
   return (
-    <div className="absolute top-full rtl:left-0 ltr:right-0 h-[10rem] bg-palette-card z-[110] shadow-md rounded-lg">
-      CartBox
+    <div className="flex flex-col absolute top-full rtl:left-0 ltr:right-0 h-[10rem] w-[20rem] bg-palette-card z-[110] shadow-md rounded-lg">
+      <header className="flex items-center justify-between text-sm font-normal p-4">
+        <span>
+          {changeNumbersFormatEnToFa(cartItemQuantity)} {t.product}
+        </span>
+        <Link href="/">
+          <a className="text-cyan-500">{t.seeCart}</a>
+        </Link>{" "}
+      </header>
     </div>
   );
 };
