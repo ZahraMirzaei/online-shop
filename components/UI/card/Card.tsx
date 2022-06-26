b829118cff28ef22126cc28270d841a286ca2b6d
ZahraMirzaei.JS <z.mirzaei2019@gmail.com>
2022-06-26 17:21:48 +0430

    feat: :sparkles: add product to cart with card action

--- components/UI/card/Card.tsx
+++ components/UI/card/Card.tsx
@@ -19,12 +19,12 @@ interface Props {
 const Card: React.FC<Props> = ({ href, product }) => {
   const { locale } = useLanguage();
   return (
-    <div className="col-span-6 sm:col-span-3 md:col-span-4 lg:col-span-3 2xl:col-span-2 shadow-xl my-4 ltr:mr-2 rtl:ml-1 md:mx-6  bg-palette-card rounded-xl flex">
+    <div className="col-span-6 sm:col-span-3 md:col-span-4 lg:col-span-3 2xl:col-span-2 shadow-xl my-4 ltr:mr-2 rtl:ml-1 md:mx-6  bg-palette-card rounded-xl flex relative">
       <Link
         href={`/${product.category[0]}/${product.category[1]}/${product.brand}/${product.slug.current}`}
       >
         <a className="flex md:items-center md:flex-col relative w-full">
-          <div className="w-full relative bg-slate-400/30 px-1 md:px-6 py-2 rounded-bl-xl rounded-tl-xl md:rounded-tr-xl md:rounded-bl-none rtl:order-2 rtl:md:order-none flex flex-col justify-between items-center">
+          <div className="w-1/2 md:w-full relative bg-slate-400/30 px-1 md:px-6 py-2 rounded-bl-xl rounded-tl-xl md:rounded-tr-xl md:rounded-bl-none rtl:order-2 rtl:md:order-none flex flex-col justify-between items-center">
             <div className="flex items-center h-full">
               <Image
                 src={urlFor(product?.image[0]).url()}
@@ -44,9 +44,8 @@ const Card: React.FC<Props> = ({ href, product }) => {
                 />
               </span>
             ) : null}
-            <CardActions />
           </div>
-          <div className="flex flex-col justify-between  flex-grow  w-[90%]  px-1 md:px-3 py-2 md:py-4">
+          <div className="flex flex-col justify-between  flex-grow  w-1/2 md:w-full  px-1 md:px-3 py-2 md:py-4">
             <div className="flex justify-center md:justify-start flex-col  flex-grow overflow-hidden">
               <div className="self-center">
                 <StarRatingComponent
@@ -93,6 +92,8 @@ const Card: React.FC<Props> = ({ href, product }) => {
           </div>
         </a>
       </Link>
+
+      <CardActions product={product} />
     </div>
   );
 };
--- components/UI/card/CardActions.tsx
+++ components/UI/card/CardActions.tsx
@@ -1,17 +1,33 @@
 import React from "react";
+import { useDispatch } from "react-redux";
+import { cartActions } from "../../../store/cart-slice";
 import { AiOutlineHeart, AiOutlineShoppingCart } from "react-icons/ai";
 import { IoShareSocialOutline } from "react-icons/io5";
+import { IProduct } from "../../../lib/types/products";
 
-const CardActions = () => {
+interface Props {
+  product: IProduct;
+}
+
+const CardActions: React.FC<Props> = ({ product }) => {
+  const dispatch = useDispatch();
+
+  function addToCartHandler() {
+    dispatch(cartActions.addItemToCart({ product: product, quantity: 1 }));
+  }
+
   return (
-    <div className="w-full md:w-auto md:h-[130px] mt-2 p-2 flex md:flex-col justify-around self-center absolute bottom-2 md:-top-2 md:bottom-auto left-0  md:-left-1 rounded-lg md:rounded-full shadow-lg backdrop-filter backdrop-blur-[8px] bg-palette-card/20  ">
+    <div className="w-1/2 md:w-auto md:h-[130px] mt-2 p-2 flex md:flex-col justify-around self-center absolute bottom-2 md:-top-2 md:bottom-auto left-0  md:-left-1 rounded-lg md:rounded-full shadow-lg backdrop-filter backdrop-blur-[8px] bg-palette-card/20  ">
       <div className="hover:text-rose-600 transition-colors sm:px-3 md:px-0">
         <AiOutlineHeart style={{ fontSize: "1.2rem" }} />
       </div>
       <div className="hover:text-rose-600 transition-colors sm:px-3 md:px-0">
         <IoShareSocialOutline style={{ fontSize: "1.2rem" }} />
       </div>
-      <div className="hover:text-rose-600 transition-colors sm:px-3 md:px-0">
+      <div
+        className="hover:text-rose-600 transition-colors sm:px-3 md:px-0"
+        onClick={addToCartHandler}
+      >
         <AiOutlineShoppingCart style={{ fontSize: "1.2rem" }} />
       </div>
     </div>
