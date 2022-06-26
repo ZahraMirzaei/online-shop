ab863e77c70d42928d08758fe84645ea6a2044a8
ZahraMirzaei.JS <z.mirzaei2019@gmail.com>
2022-06-26 10:07:35 +0430

    feat: :sparkles: create CartBox component
    to show cart items on user hover

---
+++ components/cart/CartBox.tsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+const CartBox = () => {
+  return (
+    <div className="absolute top-full rtl:left-0 ltr:right-0 h-[10rem] bg-palette-card z-[110] shadow-md rounded-lg">
+      CartBox
+    </div>
+  );
+};
+
+export default CartBox;
