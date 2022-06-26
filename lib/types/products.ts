0c42148c8f123651c8409c4dac5cf98507e5fcee
ZahraMirzaei.JS <z.mirzaei2019@gmail.com>
2022-06-26 16:44:31 +0430

    feat: :label: add some fields to IProductDetails interface and change category type

--- lib/types/products.ts
+++ lib/types/products.ts
@@ -4,23 +4,26 @@ export interface IProductDetails {
   operating_system?: string;
   ram?: string;
   ssd?: string;
-  port?: string;
+  ports?: string;
   graphic?: string;
   warranty?: string;
   back_camera?: string;
   front_camera?: string;
   battery?: string;
+  frequency_response?: string;
+  microphone?: boolean;
+  wireless?: boolean;
+  wireless_standby_time?: boolean;
+  connectionType?: string[];
+  connectors?: string[];
+  bluetooth?: boolean;
+  noise_cancelling?: boolean;
+  sound_isolating?: boolean;
 }
 
 export type TSlug = {
   _type: string;
   current: string;
-};
-
-export type TCategory = {
-  _key: string;
-  _ref: string;
-  _type: "reference";
 };
 
 export type TImage = {
@@ -42,12 +45,13 @@ export interface IProduct {
   irrdiscount?: number;
   details: IProductDetails[];
   brand: string;
-  category: TCategory[];
+  category: string[];
   isOffer: boolean;
   registerDate: string;
   timeStamp?: number;
   starRating: number;
 }
+
 export interface IProductWithSubCategory {
   image: any;
   name: string;
