2cde19e2abbd4fd047051bbc0fdc0bd99272f52f
ZahraMirzaei.JS <z.mirzaei2019@gmail.com>
2022-06-27 20:35:34 +0430

    feat: :sparkles: add cart items to CartBox and show cartItemQuantity base on locale

--- components/cart/CartBox.tsx
+++ components/cart/CartBox.tsx
@@ -1,29 +1,79 @@
 import Link from "next/link";
 import React from "react";
-import { useDispatch, useSelector } from "react-redux";
+import { useSelector } from "react-redux";
 import { useLanguage } from "../../hooks/useLanguage";
 import { ICartRootState } from "../../lib/types/cart";
 import { changeNumbersFormatEnToFa } from "../../utilities/changeNumbersFormatEnToFa";
+import { gbpCurrencyFormat } from "../../utilities/currencyFormat";
+import { ICartProduct } from "../../lib/types/cart";
+import CartItem from "./CartItem";
+import { useExchangeRateGBPToIRR } from "../../hooks/useExchangeRateGBPToIRR";
 
 const CartBox = () => {
-  const { t } = useLanguage();
-  const dispatch = useDispatch();
+  const { t, locale } = useLanguage();
+
   const cartItemQuantity = useSelector(
     (state: ICartRootState) => state.cart.totalQuantity
   );
-  const cartItems = useSelector((state: ICartRootState) => {
-    state.cart.items;
-  });
+
+  const cartTotalAmount = useSelector(
+    (state: ICartRootState) => state.cart.totalAmount
+  );
+
+  const cartItems: ICartProduct[] = useSelector(
+    (state: ICartRootState): ICartProduct[] => {
+      return state.cart.items;
+    }
+  );
+
+  const irPrice = useExchangeRateGBPToIRR(cartTotalAmount);
+
   return (
-    <div className="flex flex-col absolute top-full rtl:left-0 ltr:right-0 h-[10rem] w-[20rem] bg-palette-card z-[110] shadow-md rounded-lg">
-      <header className="flex items-center justify-between text-sm font-normal p-4">
-        <span>
-          {changeNumbersFormatEnToFa(cartItemQuantity)} {t.product}
-        </span>
-        <Link href="/">
-          <a className="text-cyan-500">{t.seeCart}</a>
-        </Link>{" "}
-      </header>
+    <div className="flex flex-col absolute top-full rtl:left-0 ltr:right-0 min-h-[15rem] max-h-[25rem] w-[20rem] bg-palette-card z-[110] shadow-md rounded-lg overflow-auto">
+      <div className="relative">
+        <header className="flex items-center justify-between sticky top-0 left-0 right-0 text-sm font-normal z-10 bg-palette-card p-2">
+          <span>
+            {locale === "en"
+              ? cartItemQuantity
+              : changeNumbersFormatEnToFa(cartItemQuantity)}{" "}
+            {t.product}
+          </span>
+          <Link href="/">
+            <a className="text-cyan-500">{t.seeCart}</a>
+          </Link>{" "}
+        </header>
+        <hr className="mt-2" />
+        <div>
+          <>
+            {cartItems.length ? (
+              cartItems.map((item) => {
+                return <CartItem key={item.slug.current} product={item} />;
+              })
+            ) : (
+              <p className="mt-20 text-center text-palette-mute font-normal">
+                {t.cartIsEmpty}
+              </p>
+            )}
+          </>
+        </div>
+        {cartItems.length ? (
+          <div className="flex items-center sticky bottom-0 left-0 right-0 bg-palette-card font-normal py-3 px-4">
+            <div className="flex flex-col flex-grow ltr:mr-2 rtl:ml-2">
+              <p className="text-sm">{t.payableAmount}</p>
+              <p className="self-end text-sm font-bold">
+                {locale === "en"
+                  ? `£ ${gbpCurrencyFormat(cartTotalAmount)}`
+                  : `تومان ${irPrice}`}
+              </p>
+            </div>
+            <Link href={"/"}>
+              <a className="py-2 px-3 bg-palette-primary text-[12px] text-palette-side rounded-lg">
+                {t.loginAndOrder}
+              </a>
+            </Link>
+          </div>
+        ) : null}
+      </div>
     </div>
   );
 };
