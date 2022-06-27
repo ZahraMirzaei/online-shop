2f94de710a3fc7491dd3d4521ce8b60f6abd54ff
ZahraMirzaei.JS <z.mirzaei2019@gmail.com>
2022-06-27 13:01:11 +0430

    feat: :sparkles: add hardRateExchange in case of fetch from exchangerate-api, returns error
    The majority of errors that can occur, are about number of requests allowed by free plan.

--- hooks/useExchangeRateGBPToIRR.tsx
+++ hooks/useExchangeRateGBPToIRR.tsx
@@ -6,15 +6,25 @@ const URL = `https://v6.exchangerate-api.com/v6/${API_KEY}/pair/GBP/IRR`;
 
 export const useExchangeRateGBPToIRR = (price: number) => {
   const [exchangePrice, setExchangePrice] = useState<number>(0);
+  const hardRateExchange = 52230.27; // each Pound into Rial
   useEffect(() => {
     const fetchExchangeRate = async () => {
       try {
         const result = await fetch(`${URL}/${price}`);
-        const data = await result.json();
-        const irToman = Math.floor(data.conversion_result / 1000) * 1000; //common currency in Iran
-        setExchangePrice(irToman);
+        if (result.ok) {
+          const data = await result.json();
+          const irToman = Math.floor(data.conversion_result / 1000) * 1000; //common currency in Iran
+          setExchangePrice(irToman);
+        } else {
+          const priceWithHardRateExchange = price * hardRateExchange;
+          const irToman = Math.floor(priceWithHardRateExchange / 1000) * 1000;
+          setExchangePrice(irToman);
+          throw new Error();
+        }
       } catch (e) {
-        console.log("Some thing is wrong in fetch data");
+        console.log(
+          `⚠ Error: the server responded with a status of 429 ()=> free account has reached the number of requests allowed by free plan and price exchange is based on hard number`
+        );
       }
     };
     if (price > 0) {
