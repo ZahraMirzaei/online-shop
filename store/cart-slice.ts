df270c64ad8317cc0bb0c0b120cca29d85a3a461
ZahraMirzaei.JS <z.mirzaei2019@gmail.com>
2022-06-27 19:22:07 +0430

    refactor: :sparkles: change cart-slice functionality and make it lighter
    use price and calculateDiscountPercentage function to calculate totalPrice and totalAmount and remove unnecessary fields from state.items, item

--- store/cart-slice.ts
+++ store/cart-slice.ts
@@ -1,6 +1,8 @@
 import { createSlice, PayloadAction } from "@reduxjs/toolkit";
 import { ICart } from "../lib/types/cart";
 import { IProduct } from "../lib/types/products";
+import { ICartProduct } from "../lib/types/cart";
+import { calculateDiscountPercentage } from "../utilities/calculateDiscountPercentage";
 
 const initialState: ICart = {
   items: [],
@@ -14,59 +16,56 @@ const cartSlice = createSlice({
   reducers: {
     addItemToCart(
       state: ICart,
-      action: PayloadAction<{ product: IProduct; quantity: number }>
+      action: PayloadAction<{ product: ICartProduct; quantity: number }>
     ) {
       const newItem = action.payload.product;
+
       const existingItem = state.items.find(
         (item) => item.slug.current === newItem.slug.current
       );
-      state.totalQuantity = state.totalQuantity + action.payload.quantity;
-      if (!existingItem) {
-        const totalEnPrice =
-          newItem.isOffer && newItem.discount
-            ? newItem.discount * action.payload.quantity
-            : newItem.price * action.payload.quantity;
 
-        const totalFaPrice =
-          newItem.isOffer && newItem.irrdiscount
-            ? newItem.irrdiscount * action.payload.quantity
-            : newItem.irrprice * action.payload.quantity;
+      state.totalQuantity = state.totalQuantity + action.payload.quantity;
+
+      state.totalAmount =
+        state.totalAmount +
+        action.payload.quantity *
+          (action.payload.product.discount
+            ? calculateDiscountPercentage(
+                action.payload.product.price,
+                action.payload.product.discount
+              )
+            : action.payload.product.price);
+
+      if (!existingItem) {
+        const totalPrice =
+          (newItem.discount
+            ? calculateDiscountPercentage(newItem.price, newItem.discount)
+            : newItem.price) * action.payload.quantity;
 
         state.items.push({
           image: newItem.image,
           name: newItem.name,
           slug: newItem.slug,
           price: newItem.price,
-          irrprice: newItem.irrprice,
           discount: newItem.discount,
-          irrdiscount: newItem.irrdiscount,
           brand: newItem.brand,
           category: newItem.category,
-          isOffer: newItem.isOffer,
-          registerDate: newItem.registerDate,
           starRating: newItem.starRating,
           quantity: action.payload.quantity,
-          totalEnPrice,
-          totalFaPrice,
+          totalPrice,
         });
       } else {
-        const totalEnPrice =
-          existingItem.isOffer && existingItem.discount
-            ? existingItem.totalEnPrice +
-              existingItem.discount * action.payload.quantity
-            : existingItem.totalEnPrice +
-              existingItem.price * action.payload.quantity;
-
-        const totalFaPrice =
-          existingItem.isOffer && existingItem.irrdiscount
-            ? existingItem.totalFaPrice +
-              existingItem.irrdiscount * action.payload.quantity
-            : existingItem.totalFaPrice +
-              existingItem.irrprice * action.payload.quantity;
+        const totalPrice =
+          existingItem.totalPrice +
+          (existingItem.discount
+            ? calculateDiscountPercentage(
+                existingItem.price,
+                existingItem.discount
+              ) * action.payload.quantity
+            : existingItem.price * action.payload.quantity);
 
         existingItem.quantity += action.payload.quantity;
-        existingItem.totalEnPrice = totalEnPrice;
-        existingItem.totalFaPrice = totalFaPrice;
+        existingItem.totalPrice = totalPrice;
       }
     },
 
@@ -78,18 +77,37 @@ const cartSlice = createSlice({
       const existingItem = state.items.find(
         (item) => item.slug.current === productSlug
       );
+
       state.totalQuantity--;
+
+      state.totalAmount =
+        state.totalAmount -
+        (existingItem?.discount
+          ? calculateDiscountPercentage(
+              existingItem.price,
+              existingItem.discount
+            )
+          : existingItem?.price)!;
+
       if (existingItem?.quantity === 1) {
         state.items = state.items.filter(
           (item) => item.slug.current !== productSlug
         );
       } else {
         existingItem!.quantity--;
-        existingItem!.totalEnPrice =
-          existingItem!.totalEnPrice - existingItem!.price;
-        existingItem!.totalFaPrice =
-          existingItem!.totalFaPrice - existingItem!.irrprice;
+        existingItem!.totalPrice =
+          existingItem!.totalPrice -
+          (existingItem?.discount
+            ? calculateDiscountPercentage(
+                existingItem.price,
+                existingItem.discount
+              )
+            : existingItem?.price)!;
       }
+    },
+
+    clearCart(state) {
+      state = initialState;
     },
   },
 });
