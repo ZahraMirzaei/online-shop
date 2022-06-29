bf6fb22af72690132437b2f89abc107b61890271
ZahraMirzaei.JS <z.mirzaei2019@gmail.com>
2022-06-29 21:31:35 +0430

    feat: :label: add pagePathsParams file for paths params type in pages file

---
+++ lib/types/pagePathsParams.ts
@@ -0,0 +1,20 @@
+export interface ICategoryPathsParams {
+  category: string;
+}
+
+export interface ISubCategoryPathsParams {
+  category: string;
+  subCategory: string;
+}
+
+export interface ITitlePathsParams {
+  subCategory: string;
+  title: string;
+}
+
+export interface ISlugPathsParams {
+  slug: { current: string };
+  category: string;
+  subCategory: string;
+  title: string;
+}
