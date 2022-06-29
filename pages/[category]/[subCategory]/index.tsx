a1a49c289fc38031cc694a0f32d3585611a7de22
ZahraMirzaei.JS <z.mirzaei2019@gmail.com>
2022-06-29 21:52:39 +0430

    feat: :sparkles: define getStaicPaths in correct way and use ISubCategoryPathsParams in [subCategory] page

--- pages/[category]/[subCategory]/index.tsx
+++ pages/[category]/[subCategory]/index.tsx
@@ -3,6 +3,7 @@ import { GetStaticProps, GetStaticPaths } from "next";
 import { client } from "../../../lib/client";
 import { IProduct } from "../../../lib/types/products";
 import ProductList from "../../../components/productList/ProductList";
+import { ISubCategoryPathsParams } from "../../../lib/types/pagePathsParams";
 
 const subCategory: NextPage<{
   products: IProduct[];
@@ -16,47 +17,33 @@ const subCategory: NextPage<{
 
 export default subCategory;
 
+export const getStaticPaths: GetStaticPaths = async () => {
+  const query = `*[_type=="product"]{
+    "category":category[0],
+    "subCategory":category[1],
+  }`;
+  const products = await client.fetch(query);
+  const paths = products.map((product: ISubCategoryPathsParams) => ({
+    params: {
+      category: product.category.toString(),
+      subCategory: product.subCategory.toString(),
+    },
+  }));
+  return {
+    fallback: "blocking",
+    paths,
+  };
+};
+
 export const getStaticProps: GetStaticProps = async (context) => {
   const subCategory = context.params?.subCategory;
   const category = context.params?.category;
-  const productQuery = `*[_type=='product'&& category[0]=="${category}" &&category[1]=="${subCategory}"]`;
+  const productQuery = `*[_type=='product'&& category[0]=="${category}" && category[1]=="${subCategory}"]`;
   const products = await client.fetch(productQuery);
 
   return {
     props: {
       products: products,
-    }, // will be passed to the page component as props
-  };
-};
-
-export const getStaticPaths: GetStaticPaths = async () => {
-  return {
-    fallback: true,
-    paths: [
-      {
-        params: {
-          category: "digital",
-          subCategory: "laptop",
-        },
-      },
-      {
-        params: {
-          category: "digital",
-          subCategory: "mobile",
-        },
-      },
-      {
-        params: {
-          category: "digital",
-          subCategory: "computer",
-        },
-      },
-      {
-        params: {
-          category: "digital",
-          subCategory: "other",
-        },
-      },
-    ],
+    },
   };
 };
