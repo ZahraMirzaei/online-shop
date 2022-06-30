d684b61df497ae85b9060a4d8584850b6c6f77b4
ZahraMirzaei.JS <z.mirzaei2019@gmail.com>
2022-06-30 21:06:40 +0430

    feat: :sparkles: add OrderSummaryBox component to cart page

--- pages/cart.tsx
+++ pages/cart.tsx
@@ -1,12 +1,16 @@
 import type { NextPage } from "next";
 import CartList from "../components/cart/CartList";
 import Breadcrumb from "../components/UI/Breadcrumb";
+import OrderSummaryBox from "../components/cart/OrderSummaryBox";
 
 const cart: NextPage = () => {
   return (
     <div>
       <Breadcrumb />
-      <CartList />
+      <div className="flex justify-center flex-col md:flex-row items-start relative">
+        <CartList />
+        <OrderSummaryBox />
+      </div>
     </div>
   );
 };
