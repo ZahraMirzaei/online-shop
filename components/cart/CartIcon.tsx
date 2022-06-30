c9324f7b5c6bff196c0a4ce2b595082b60b693e1
ZahraMirzaei.JS <z.mirzaei2019@gmail.com>
2022-06-30 16:51:04 +0430

    feat: :sparkles: linked CartIcon to cart page

--- components/cart/CartIcon.tsx
+++ components/cart/CartIcon.tsx
@@ -31,7 +31,7 @@ const Basket = () => {
       onMouseOver={() => onMouseHoverHandler(true)}
       onMouseOut={() => onMouseHoverHandler(false)}
     >
-      <Link href="/">
+      <Link href="/cart">
         <a className="relative flex items-center ltr:md:pl-6 rtl:md:pr-6 rtl:md:border-r-2 rtl:md:border-r-slate-300 ltr:md:border-l-2 ltr:md:border-l-slate-300 z-50">
           <AiOutlineShoppingCart style={{ fontSize: "1.6rem" }} />
           <span className="absolute -top-3 -right-[0.3rem] rtl:md:right-[1rem]  flex items-center justify-center w-5 h-5 rtl:pt-[0.1rem] rounded-full bg-palette-primary text-[0.75rem] leading-3 text-white shadow-lg">
