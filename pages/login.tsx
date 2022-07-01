9bb4a77365e79bdcdfd1fc080a60e0fd5b812b5c
ZahraMirzaei.JS <z.mirzaei2019@gmail.com>
2022-07-01 19:42:22 +0430

    feat: :sparkles: create login page

---
+++ pages/login.tsx
@@ -0,0 +1,7 @@
+import type { NextPage } from "next";
+import EnteringBox from "../components/entering/EnteringBox";
+const login: NextPage = () => {
+  return <EnteringBox title="login" />;
+};
+
+export default login;
