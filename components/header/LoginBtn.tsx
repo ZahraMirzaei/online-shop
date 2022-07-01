449fb0314e796bdd4ab3c3d22ca456f047b93e10
ZahraMirzaei.JS <z.mirzaei2019@gmail.com>
2022-07-01 19:44:43 +0430

    feat: :sparkles: wrap login button with link and set href to /login page

--- components/header/LoginBtn.tsx
+++ components/header/LoginBtn.tsx
@@ -1,6 +1,7 @@
 import React from "react";
 import { useLanguage } from "../../hooks/useLanguage";
 import { HiOutlineLogin } from "react-icons/hi";
+import Link from "next/link";
 
 interface Props {
   modifier?: string;
@@ -8,18 +9,20 @@ interface Props {
 const LoginBtn: React.FC<Props> = ({ modifier }) => {
   const { t } = useLanguage();
   return (
-    <>
-      {modifier === "md" ? (
-        <div className="flex items-center rounded-lg  py-1 px-2 ltr:mr-3 rtl:ml-3 border-[1px] border-gray-200 dark:border-gray-200/40 shadow-sm ">
+    <Link href={"/login"}>
+      <a>
+        {modifier === "md" ? (
+          <div className="flex items-center rounded-lg  py-1 px-2 ltr:mr-3 rtl:ml-3 border-[1px] border-gray-200 dark:border-gray-200/40 shadow-sm ">
+            <HiOutlineLogin style={{ fontSize: "1.6rem" }} />
+            <p className="ltr:ml-2 rtl:mr-2 text-xs">
+              {t.login} | {t.signUp}
+            </p>
+          </div>
+        ) : (
           <HiOutlineLogin style={{ fontSize: "1.6rem" }} />
-          <p className="ltr:ml-2 rtl:mr-2 text-xs">
-            {t.login} | {t.signIn}
-          </p>
-        </div>
-      ) : (
-        <HiOutlineLogin style={{ fontSize: "1.6rem" }} />
-      )}
-    </>
+        )}
+      </a>
+    </Link>
   );
 };
 
