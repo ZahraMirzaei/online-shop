c1e96a46f156cc57bc2f7a98d47f0ad20ef8e1f2
ZahraMirzaei.JS <z.mirzaei2019@gmail.com>
2022-07-01 19:46:31 +0430

    feat: :sparkles: create signUp page

---
+++ pages/signUp.tsx
@@ -0,0 +1,7 @@
+import type { NextPage } from "next";
+import EnteringBox from "../components/entering/EnteringBox";
+const signUp: NextPage = () => {
+  return <EnteringBox title="signUp" />;
+};
+
+export default signUp;
