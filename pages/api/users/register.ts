25ca2177fc6adcc6738e99ba23fb3d028ccc788e
ZahraMirzaei.JS <z.mirzaei2019@gmail.com>
2022-07-03 10:50:54 +0430

    feat: :sparkles: create register api to post sign up user data to sanity

---
+++ pages/api/users/register.ts
@@ -0,0 +1,56 @@
+import { NextApiRequest, NextApiResponse } from "next";
+import nc from "next-connect";
+import bcrypt from "bcryptjs";
+import axios from "axios";
+import config from "../../../lib/config";
+import { client } from "../../../lib/client";
+import { signToken } from "../../../utilities/auth";
+
+const handler = nc();
+
+handler.post(async (req: NextApiRequest, res: NextApiResponse) => {
+  const projectId = config.projectId;
+  const dataset = config.dataset;
+  const tokenWithWriteAccess = process.env.NEXT_PUBLIC_SANITY_TOKEN;
+  const createMutations = [
+    {
+      create: {
+        _type: "user",
+        name: req.body.name,
+        email: req.body.email,
+        password: bcrypt.hashSync(req.body.password),
+        isAdmin: false,
+      },
+    },
+  ];
+  const existUser = await client.fetch(
+    `*[_type == "user" && email == $email][0]`,
+    {
+      email: req.body.email,
+    }
+  );
+  if (existUser) {
+    return res.status(401).send({ message: "Email already exists" });
+  }
+  const { data } = await axios.post(
+    `https://${projectId}.api.sanity.io/v2021-06-07/data/mutate/${dataset}?returnIds=true`,
+    { mutations: createMutations },
+    {
+      headers: {
+        "Content-type": "application/json",
+        Authorization: `Bearer ${tokenWithWriteAccess}`,
+      },
+    }
+  );
+  const userId = data.results[0].id;
+  const user = {
+    _id: userId,
+    name: req.body.name,
+    email: req.body.email,
+    isAdmin: false,
+  };
+  const token = signToken(user);
+  res.send({ ...user, token });
+});
+
+export default handler;
