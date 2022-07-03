0d31a8175ad562e9977248c04bfff109cdbb8351
ZahraMirzaei.JS <z.mirzaei2019@gmail.com>
2022-07-03 17:55:33 +0430

    refactor: :recycle: change moon icon and style

--- components/header/Theme.tsx
+++ components/header/Theme.tsx
@@ -1,9 +1,7 @@
 import React, { useState, useEffect } from "react";
 import { useTheme } from "next-themes";
-// import { FaCloudMoon } from "react-icons/fa";
-import { FaMoon } from "react-icons/fa";
 import { BsFillBrightnessHighFill } from "react-icons/bs";
-
+import { RiMoonFill } from "react-icons/ri";
 const Theme = () => {
   const [mounted, setMounted] = useState(false);
 
@@ -30,10 +28,11 @@ const Theme = () => {
     } else {
       return (
         <div role="button" onClick={() => setTheme("dark")}>
-          <FaMoon
+          <RiMoonFill
             style={{
-              fontSize: "1.1rem",
-              filter: "drop-shadow(0px 2px 3px rgb(0 0 0 / 0.4))",
+              fontSize: "1.3rem",
+              filter: "drop-shadow(0px 1px 2px rgb(0 0 0 / 0.3))",
+              color: "#0f172a",
             }}
           />
         </div>
@@ -41,11 +40,7 @@ const Theme = () => {
     }
   };
 
-  return (
-    <div className="rtl:ml-1 ltr:ml-1  shadow-md p-1 rounded-full">
-      {renderThemeChanger()}
-    </div>
-  );
+  return <div className="rtl:ml-1 ltr:ml-1 p-1">{renderThemeChanger()}</div>;
 };
 
 export default Theme;
