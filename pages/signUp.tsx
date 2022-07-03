bb58086863ba875cd6d8a3b4f0664e8afdc4dba7
ZahraMirzaei.JS <z.mirzaei2019@gmail.com>
2022-07-03 11:11:53 +0430

    feat: :sparkles: add logic to signUp page
    send data to register api and set cookie for userInfo

--- pages/signUp.tsx
+++ pages/signUp.tsx
@@ -1,7 +1,48 @@
 import type { NextPage } from "next";
+import { useDispatch, useSelector } from "react-redux";
+import { userInfoActions } from "../store/user-slice";
+import jsCookie from "js-cookie";
 import EnteringBox from "../components/entering/EnteringBox";
-const signUp: NextPage = () => {
-  return <EnteringBox title="signUp" />;
+import { IUser } from "../lib/types/user";
+import axios from "axios";
+import { getError } from "../utilities/error";
+import { useRouter } from "next/router";
+import { useEffect } from "react";
+import { IUserInfoRootState } from "../lib/types/user";
+const SignUp: NextPage = () => {
+  const dispatch = useDispatch();
+  const router = useRouter();
+  const { redirect } = router.query;
+  const userInfo = useSelector(
+    (state: IUserInfoRootState) => state.userInfo.userInformation
+  );
+  useEffect(() => {
+    if (userInfo) {
+      router.push((redirect as string) || "/");
+    }
+  }, [userInfo, redirect, router]);
+  async function signUpHandler(user: IUser) {
+    const { name, email, password } = user;
+    try {
+      const { data } = await axios.post("/api/users/register", {
+        name,
+        email,
+        password,
+      });
+      dispatch(userInfoActions.userLogin(data));
+      jsCookie.set("userInfo", JSON.stringify(data));
+      router.push("/");
+    } catch (err: any) {
+      /* sanity.io is boycott for the people from Iran so I set cookies for whom don't use VPN in Iran*/
+      if (err.response.data.status == 500) {
+        dispatch(userInfoActions.userLogin(user));
+        jsCookie.set("userInfo", JSON.stringify(user));
+      }
+      console.log(getError(err));
+      router.push("/");
+    }
+  }
+  return <EnteringBox title="signUp" submitHandler={signUpHandler} />;
 };
 
-export default signUp;
+export default SignUp;
