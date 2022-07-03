e5f8089fb4ef37e9372e880b7340288f0a85a7f9
ZahraMirzaei.JS <z.mirzaei2019@gmail.com>
2022-07-03 11:13:28 +0430

    refactor: :truck: move LoginBtn to user folder

---
+++ components/header/user/LoginBtn.tsx
@@ -0,0 +1,25 @@
+import React from "react";
+import { useLanguage } from "../../../hooks/useLanguage";
+import { HiOutlineLogin } from "react-icons/hi";
+import Link from "next/link";
+
+const LoginBtn = () => {
+  const { t } = useLanguage();
+  return (
+    <Link href={"/login"}>
+      <a>
+        <div className="hidden md:flex items-center rounded-lg  py-1 px-2 ltr:mr-3 rtl:ml-3 border-[1px] border-gray-200 dark:border-gray-200/40 shadow-sm ">
+          <HiOutlineLogin style={{ fontSize: "1.6rem" }} />
+          <p className="ltr:ml-2 rtl:mr-2 text-xs">
+            {t.login} | {t.signUp}
+          </p>
+        </div>
+        <div className="md:hidden">
+          <HiOutlineLogin style={{ fontSize: "1.6rem" }} />
+        </div>
+      </a>
+    </Link>
+  );
+};
+
+export default LoginBtn;
