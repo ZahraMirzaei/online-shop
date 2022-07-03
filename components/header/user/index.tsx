baf16188cda836cddfb1de5f2a6bfe511711c3df
ZahraMirzaei.JS <z.mirzaei2019@gmail.com>
2022-07-03 16:03:39 +0430

    feat: :sparkles: create User compoenent
    wrap UserAccountBtn and LoginBtn and show them base on user is login or not.

---
+++ components/header/user/index.tsx
@@ -0,0 +1,14 @@
+import React from "react";
+import { useSelector } from "react-redux";
+import { IUserInfoRootState } from "../../../lib/types/user";
+import UserAccountBtn from "./UserAccountBtn";
+import LoginBtn from "./LoginBtn";
+
+const User = () => {
+  const userInfo = useSelector(
+    (state: IUserInfoRootState) => state.userInfo.userInformation
+  );
+  return <div>{userInfo ? <UserAccountBtn /> : <LoginBtn />}</div>;
+};
+
+export default User;
