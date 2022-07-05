abca50b4e0c5501737311c2f28ec6b48b2f1b223
ZahraMirzaei.JS <z.mirzaei2019@gmail.com>
2022-07-05 12:41:09 +0430

    feat: :sparkles: different language box display in small and large device, replace LanguageItem instead of its code in Language component

--- components/header/language/Language.tsx
+++ components/header/language/Language.tsx
@@ -1,38 +1,47 @@
 import React, { useEffect, useState } from "react";
-import Link from "next/link";
-import { useRouter } from "next/router";
+import { useDispatch } from "react-redux";
+import { settingBoxActions } from "../../../store/settingBox-slice";
 import { MdLanguage } from "react-icons/md";
-import { HiChevronUp, HiChevronDown } from "react-icons/hi";
 import { useLanguage } from "../../../hooks/useLanguage";
+import LanguageItem from "./LanguageItem";
+
 const Language = () => {
   const { t, locale } = useLanguage();
-  const [lang, setLang] = useState(locale);
-  const route = useRouter();
-
+  const dispatch = useDispatch();
   const [openLang, setOpenLang] = useState(false);
-  let ArrowDirection = !openLang ? HiChevronDown : HiChevronUp;
 
   useEffect(() => {
     document.documentElement.dir = locale === "en" ? "ltr" : "rtl";
   }, [locale]);
 
-  function onChangeHandler(e: React.ChangeEvent<HTMLInputElement>) {
-    setLang(e.currentTarget.id);
+  function onCloseLangBox(isOpen: boolean) {
+    setOpenLang(isOpen);
   }
 
   return (
     <div className="relative rtl:ml-2 rtl:pl-2 ltr:mr-2 ltr:pr-2">
+      <div className="md:hidden">
+        <h3>{t.language}</h3>
+        <div className={`ltr:ml-2 rtl:mr-2 mt-2 z-10`}>
+          <LanguageItem
+            language="en"
+            onCloseBox={() => dispatch(settingBoxActions.closeSettingBox())}
+          />
+          <LanguageItem
+            language="fa"
+            onCloseBox={() => dispatch(settingBoxActions.closeSettingBox())}
+          />
+        </div>
+      </div>
       <div
-        className="flex items-center cursor-pointer"
+        className="hidden md:flex items-center cursor-pointer"
         onClick={() => setOpenLang((prevState) => !prevState)}
       >
-        <div className="flex items-center">
-          <p className="mx-[0.3rem] text-sm font-bold font-english">
-            {locale === "en" ? "En" : "Fa"}
-          </p>
+        <p className="mx-[0.3rem] text-sm font-bold font-english">
+          {locale === "en" ? "En" : "Fa"}
+        </p>
 
-          <MdLanguage style={{ fontSize: "1.4rem" }} />
-        </div>
+        <MdLanguage style={{ fontSize: "1.4rem" }} />
       </div>
       {openLang ? (
         <>
@@ -43,57 +52,8 @@ const Language = () => {
           <div
             className={`absolute top-6 ltr:right-0 rtl:left-0 bg-palette-card py-3 px-6 shadow-md rounded-md z-10`}
           >
-            <Link href={`${route.asPath}`} locale="fa">
-              <a className="whitespace-nowrap flex justify-between items-center">
-                <div
-                  className="flex items-center"
-                  onClick={() => setOpenLang(false)}
-                >
-                  <input
-                    type="radio"
-                    id="fa"
-                    name="language"
-                    value={lang}
-                    className="hidden md:block accent-rose-600"
-                    checked={locale === "fa" ? true : false}
-                    onChange={onChangeHandler}
-                  />
-                  <label
-                    htmlFor="fa"
-                    className={`font-farsi mx-3 grow ${
-                      locale === "fa" && "font-bold"
-                    }`}
-                  >
-                    {t.farsi}
-                  </label>
-                </div>
-              </a>
-            </Link>
-
-            <Link href={`${route.asPath}`} locale="en">
-              <a className="whitespace-nowrap flex justify-start items-center mt-3">
-                <div
-                  className="flex items-center"
-                  onClick={() => setOpenLang(false)}
-                >
-                  <input
-                    type="radio"
-                    id="en"
-                    name="language"
-                    value={lang}
-                    className="hidden md:block accent-rose-600"
-                    checked={locale === "en" ? true : false}
-                    onChange={onChangeHandler}
-                  />
-                  <label
-                    htmlFor="en"
-                    className={`mx-3 grow ${locale === "en" && "font-bold"}`}
-                  >
-                    {t.english}
-                  </label>
-                </div>
-              </a>
-            </Link>
+            <LanguageItem language="fa" onCloseBox={onCloseLangBox} />
+            <LanguageItem language="en" onCloseBox={onCloseLangBox} />
           </div>
         </>
       ) : null}
