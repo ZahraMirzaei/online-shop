4802ec582c83428e2d5ffbf69809fe91292de08d
ZahraMirzaei.JS <z.mirzaei2019@gmail.com>
2022-07-05 12:41:58 +0430

    feat: :sparkles: use settingBox-slice to show settingBox

--- components/header/Settings.tsx
+++ components/header/Settings.tsx
@@ -1,17 +1,44 @@
 import React from "react";
 import { AiOutlineSetting } from "react-icons/ai";
+import { useDispatch, useSelector } from "react-redux";
+import { settingBoxActions } from "../../store/settingBox-slice";
 import Theme from "./Theme";
 import Language from "./language/Language";
+import { ISettingBoxRootState } from "../../lib/types/settingBox";
 
 const Settings = () => {
+  const dispatch = useDispatch();
+
+  const isSettingBoxOpen = useSelector(
+    (state: ISettingBoxRootState) => state.settingBox.isOpen
+  );
+
+  function toggleShowSettingBox() {
+    dispatch(settingBoxActions.toggleSettingBox());
+  }
+
+  function onCloseSettingBox() {
+    dispatch(settingBoxActions.closeSettingBox());
+  }
+
   return (
     <div className="relative flex justify-between items-center z-[150]">
-      <AiOutlineSetting style={{ fontSize: "1.5rem" }} />
-      <div className="absolute top-full ltr:right-0 rtl:left-0 bg-palette-card shadow-md rounded-lg px-3 py-3 ">
-        <Language />
-        <hr />
-        <Theme />
+      <div onClick={toggleShowSettingBox}>
+        <AiOutlineSetting style={{ fontSize: "1.5rem" }} />
       </div>
+      {isSettingBoxOpen ? (
+        <>
+          <div
+            className="fixed inset-0 -z-1 bg-black/20"
+            onClick={onCloseSettingBox}
+          ></div>
+          <div className="absolute top-full ltr:right-0 rtl:left-0 bg-palette-card shadow-md rounded-lg px-3 py-3 ">
+            <Language />
+            <hr />
+            <Theme />
+          </div>
+        </>
+      ) : null}
     </div>
   );
 };
