ce45c676114c4b25b4bfc67f3ddea1af4d705e69
ZahraMirzaei.JS <z.mirzaei2019@gmail.com>
2022-07-06 20:53:44 +0430

    feat: :sparkles: add functionality of favorite-slice actions to CardActions component

--- components/UI/card/CardActions.tsx
+++ components/UI/card/CardActions.tsx
@@ -1,10 +1,15 @@
 import React from "react";
-import { useDispatch } from "react-redux";
+import { useDispatch, useSelector } from "react-redux";
 import { cartActions } from "../../../store/cart-slice";
-import { AiOutlineHeart, AiOutlineShoppingCart } from "react-icons/ai";
-import { IoShareSocialOutline } from "react-icons/io5";
+import { favoriteActions } from "../../../store/favorite-slice";
+import {
+  RiHeartFill,
+  RiHeartAddLine,
+  RiShareLine,
+  RiShoppingCart2Line,
+} from "react-icons/ri";
 import { IProduct } from "../../../lib/types/products";
-import { ICartProduct } from "../../../lib/types/cart";
+import { IFavoriteRootState } from "../../../lib/types/favorite";
 
 interface Props {
   product: IProduct;
@@ -13,38 +18,49 @@ interface Props {
 const CardActions: React.FC<Props> = ({ product }) => {
   const dispatch = useDispatch();
 
-  const productInfoAddToCart: ICartProduct = {
-    image: product.image,
-    name: product.name,
-    slug: product.slug,
-    price: product.price,
-    discount: product.discount ? product.discount : undefined,
-    brand: product.brand,
-    category: product.category,
-    starRating: product.starRating,
-    quantity: 1,
-    totalPrice: product.price,
-  };
+  const favoriteItems = useSelector(
+    (state: IFavoriteRootState) => state.favorite.items
+  );
+  const isInFavorite = favoriteItems.some(
+    (item) => item.slug.current === product.slug.current
+  );
+  const FavoriteIcon = isInFavorite ? RiHeartFill : RiHeartAddLine;
 
   function addToCartHandler() {
-    dispatch(
-      cartActions.addItemToCart({ product: productInfoAddToCart, quantity: 1 })
-    );
+    dispatch(cartActions.addItemToCart({ product: product, quantity: 1 }));
+  }
+
+  function toggleFavoriteHandler() {
+    !isInFavorite
+      ? dispatch(favoriteActions.addToFavorite(product))
+      : dispatch(favoriteActions.removeFromFavorite(product.slug.current));
   }
 
   return (
     <div className="w-1/2 md:w-auto md:h-[130px] mt-2 p-2 flex md:flex-col justify-around self-center absolute bottom-2 md:-top-2 md:bottom-auto left-0  md:-left-1 rounded-lg md:rounded-full shadow-lg backdrop-filter backdrop-blur-[8px] bg-palette-card/20  ">
-      <div className="hover:text-rose-600 transition-colors sm:px-3 md:px-0">
-        <AiOutlineHeart style={{ fontSize: "1.2rem" }} />
+      <div
+        className="hover:text-rose-600 transition-colors sm:px-3 md:px-0"
+        onClick={toggleFavoriteHandler}
+      >
+        <FavoriteIcon
+          style={{
+            fontSize: "1.2rem",
+            fill: `${isInFavorite ? "#ee384e" : ""}`,
+          }}
+        />
       </div>
       <div className="hover:text-rose-600 transition-colors sm:px-3 md:px-0">
-        <IoShareSocialOutline style={{ fontSize: "1.2rem" }} />
+        <RiShareLine style={{ fontSize: "1.2rem" }} />
       </div>
       <div
         className="hover:text-rose-600 transition-colors sm:px-3 md:px-0"
         onClick={addToCartHandler}
       >
-        <AiOutlineShoppingCart style={{ fontSize: "1.2rem" }} />
+        <RiShoppingCart2Line
+          style={{
+            fontSize: "1.2rem",
+          }}
+        />
       </div>
     </div>
   );
