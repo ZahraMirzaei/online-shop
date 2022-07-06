7c5c3ce321cb589339e9891acbe3cee347708a65
ZahraMirzaei.JS <z.mirzaei2019@gmail.com>
2022-07-06 08:16:48 +0430

    refactor: :recycle: put SideBarMenu and MegaMenu in menu/index and import index to header

---
+++ components/header/menu/index.tsx
@@ -0,0 +1,14 @@
+import React from "react";
+import SideBarMenu from "./SideBarMenu";
+import MegaMenu from "./MegaMenu";
+
+const index = () => {
+  return (
+    <>
+      <SideBarMenu />
+      <MegaMenu />
+    </>
+  );
+};
+
+export default index;
