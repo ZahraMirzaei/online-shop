3bf9720fb2013f7d797a9cd94063c3b9dd59161b
ZahraMirzaei.JS <z.mirzaei2019@gmail.com>
2022-07-06 20:45:25 +0430

    feat: :sparkles: create Favorite component to wrapp favorite items in favorite page

---
+++ components/favorite/index.tsx
@@ -0,0 +1,30 @@
+import React from "react";
+import { useSelector } from "react-redux";
+import { useLanguage } from "../../hooks/useLanguage";
+import { IFavoriteRootState } from "../../lib/types/favorite";
+import FavoriteItem from "./FavoriteItem";
+
+const Favorites = () => {
+  const { t } = useLanguage();
+  const favoriteItems = useSelector(
+    (state: IFavoriteRootState) => state.favorite.items
+  );
+  return (
+    <div className="max-w-[1700px] mx-auto">
+      {favoriteItems.length ? (
+        <div className="grid gap-4 grid-cols-6 lg:grid-cols-12">
+          {favoriteItems.map((favoriteItem) => (
+            <FavoriteItem
+              key={favoriteItem.slug.current}
+              product={favoriteItem}
+            />
+          ))}
+        </div>
+      ) : (
+        <p>{t.thereAreNoFavorites}</p>
+      )}
+    </div>
+  );
+};
+
+export default Favorites;
