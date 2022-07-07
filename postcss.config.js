41201001d463a62a253b677e6bf7b7954a5df1d8
ZahraMirzaei.JS <z.mirzaei2019@gmail.com>
2022-07-07 11:40:32 +0430

    fix: :bug: change postcss.config.js file and add postcss-import package for support @import in css files

--- postcss.config.js
+++ postcss.config.js
@@ -1,19 +1,7 @@
 module.exports = {
-  plugins: [
-    require("tailwindcss"),
-    require("autoprefixer"),
-    process.env.NODE_ENV === "production"
-      ? [
-          require("@fullhuman/postcss-purgecss"),
-          {
-            content: [
-              "./pages/**/*.{js,jsx,ts,tsx}",
-              "./components/**/*.{js,jsx,ts,tsx}",
-            ],
-            defaultExtractor: (content) =>
-              content.match(/[\w-/:]+(?<!:)/g) || [],
-          },
-        ]
-      : undefined,
-  ],
+  plugins: {
+    "postcss-import": {},
+    tailwindcss: {},
+    autoprefixer: {},
+  },
 };
