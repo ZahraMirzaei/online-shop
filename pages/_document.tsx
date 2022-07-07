62bf67bc4e494e1cf67bcd0f987dc270838cc0ed
ZahraMirzaei.JS <z.mirzaei2019@gmail.com>
2022-07-07 21:38:43 +0430

    feat: :sparkles: add favicon to project

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -13,6 +13,7 @@ export default function Document() {
           href="https://fonts.googleapis.com/css2?family=Poppins:wght@100;300;400;500&display=swap"
           rel="stylesheet"
         />
+        <link rel="shortcut icon" href="/images/zishop.ico" />
       </Head>
       <body className="px-5 xl:px-16">
         <Main />
