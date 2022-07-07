7a5764a01d5d2aaf1d649cfd3d2af92e734f5dbe
ZahraMirzaei.JS <z.mirzaei2019@gmail.com>
2022-07-07 20:50:25 +0430

    fix: :bug: add mode:jit to tailwind.config.js file to support tailwind classes in all files that has been specified in content field

--- tailwind.config.js
+++ tailwind.config.js
@@ -10,6 +10,7 @@ function withOpacity(variableName) {
 
 module.exports = {
   darkMode: "class",
+  mode: "jit",
   content: [
     "./pages/**/*.{js,ts,jsx,tsx}",
     "./components/**/*.{js,ts,jsx,tsx}",
