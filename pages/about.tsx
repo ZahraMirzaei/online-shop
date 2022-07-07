d088e2aef9b5c7fb107b13802b6971d76a016043
ZahraMirzaei.JS <z.mirzaei2019@gmail.com>
2022-07-07 09:44:52 +0430

    feat: :sparkles: add content to about page

--- pages/about.tsx
+++ pages/about.tsx
@@ -1,7 +1,15 @@
 import type { NextPage } from "next";
+import { useLanguage } from "../hooks/useLanguage";
 
-const about: NextPage = () => {
-  return <div>about</div>;
+const About: NextPage = () => {
+  const { t } = useLanguage();
+  return (
+    <div className="w-full lg:w-1/2 mt-8 md:mt-0 px-4 sm:px-8 md:px-0">
+      <p className="leading-8 text-justify">{t.aboutLongText}</p>
+      <p className="my-4">{t.aboutEnjoy}</p>
+      <p>{t.myName}</p>
+    </div>
+  );
 };
 
-export default about;
+export default About;
