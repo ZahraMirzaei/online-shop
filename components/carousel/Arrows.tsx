758647af933722ea5baa02cc4deda14d66062841
ZahraMirzaei.JS <z.mirzaei2019@gmail.com>
2022-07-08 19:22:19 +0430

    style: :lipstick: change slider arrow (::before) content and change arrow's width in medium devices

--- components/carousel/Arrows.tsx
+++ components/carousel/Arrows.tsx
@@ -14,7 +14,7 @@ export const NextArrow: React.FC<Props> = ({
 }) => {
   return (
     <div
-      className={`${className} z-10 w-12 lg:w-28 h-full pt-10 !flex items-center justify-center ltr:left-auto ltr:right-0 rtl:right-auto rtl:left-0  hover:bg-palette-card/10 drop-shadow-lg before:content-['']`}
+      className={`${className} z-10 w-12 md:w-16 lg:w-28 h-full pt-10 !flex items-center justify-center ltr:left-auto ltr:right-0 rtl:right-auto rtl:left-0  hover:bg-palette-card/10 drop-shadow-lg before:content-['']`}
       style={{ ...style }}
       onClick={onClick}
       aria-label={to}
@@ -29,7 +29,7 @@ export const PrevArrow: React.FC<Props> = ({
 }) => {
   return (
     <div
-      className={`${className} z-10 w-12 lg:w-28 h-full pt-10 !flex items-center justify-center ltr:left-0 ltr:right-auto rtl:right-0 rtl:left-auto before:text-[20px] lg:before:text-[30px] before:content-[''] hover:bg-palette-card/10 drop-shadow-lg`}
+      className={`${className} z-10 w-12 md:w-16 lg:w-28 h-full pt-10 !flex items-center justify-center ltr:left-0 ltr:right-auto rtl:right-0 rtl:left-auto before:text-[20px] lg:before:text-[30px] before:content-[''] hover:bg-palette-card/10 drop-shadow-lg`}
       style={{ ...style }}
       onClick={onClick}
       aria-label={to}
