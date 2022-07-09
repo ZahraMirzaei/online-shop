830db88ddb8dc50045d807573f1796d47a3a7998
ZahraMirzaei.JS <z.mirzaei2019@gmail.com>
2022-07-09 18:37:48 +0430

    feat: :sparkles: add aout-me image to About page
    show image in large screen

--- pages/about.tsx
+++ pages/about.tsx
@@ -1,13 +1,25 @@
 import type { NextPage } from "next";
 import { useLanguage } from "../hooks/useLanguage";
+import Image from "next/image";
 
 const About: NextPage = () => {
   const { t } = useLanguage();
   return (
-    <div className="w-full lg:w-1/2 mt-8 md:mt-0 px-4 sm:px-8 md:px-0">
-      <p className="leading-8 text-justify">{t.aboutLongText}</p>
-      <p className="my-4">{t.aboutEnjoy}</p>
-      <p>{t.myName}</p>
+    <div className="flex">
+      <div className="w-full lg:w-1/2 mt-8 md:mt-0 px-4 sm:px-8 md:px-0">
+        <p className="leading-8 md:text-justify">{t.aboutLongText}</p>
+        <p className="my-4">{t.aboutEnjoy}</p>
+        <p>{t.myName}</p>
+      </div>
+      <div className="hidden md:block flex-grow text-center">
+        <Image
+          src="/images/about-me.svg"
+          alt="about me"
+          width={500}
+          height={500}
+          className="object-contain"
+        />
+      </div>
     </div>
   );
 };
