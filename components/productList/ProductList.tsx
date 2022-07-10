1d263ab537c9b2b544226da2f5549bc216efc7fa
ZahraMirzaei.JS <z.mirzaei2019@gmail.com>
2022-07-10 20:38:20 +0430

    refactor: :fire: remove unnecessary code
    the product list is sorted on newestProducts page and then sent to ProductList component, so it's unnecessary that sort it again. it helps to reduce the number of rendering productList component

--- components/productList/ProductList.tsx
+++ components/productList/ProductList.tsx
@@ -5,10 +5,9 @@ import SubmenuCategory from "./SubmenuCategory";
 import Card from "../UI/card/Card";
 import Breadcrumb from "../UI/Breadcrumb";
 import Sort from "./Sort";
-import { useDispatch, useSelector } from "react-redux";
+import { useDispatch } from "react-redux";
 import { SortedProductsListActions } from "../../store/sortedProductList-slice";
 import { useRouter } from "next/router";
-import { IProductListRootState } from "../../lib/types/productList";
 
 interface Props {
   productList: IProduct[];
@@ -16,9 +15,10 @@ interface Props {
 const ProductList: React.FC<Props> = ({ productList }) => {
   const router = useRouter();
   const { t } = useLanguage();
-  const firstSortBased =
-    router.pathname === "/newestProducts" ? "newestProducts" : "all";
-  const [selectedRadioBtn, setSelectedRadioBtn] = useState(firstSortBased);
+  let isInNewestProductsPage =
+    router.pathname === "/newestProducts" ? true : false;
+
+  const [selectedRadioBtn, setSelectedRadioBtn] = useState<string>("all");
   const dispatch = useDispatch();
 
   useEffect(() => {
@@ -30,33 +30,24 @@ const ProductList: React.FC<Props> = ({ productList }) => {
     );
   }, [dispatch, productList, selectedRadioBtn]);
 
-  const productsSortedList = useSelector(
-    (state: IProductListRootState) => state.sortedProductsList.productsList
-  );
-
   function onChangeHandler(e: React.ChangeEvent<HTMLInputElement>) {
     setSelectedRadioBtn(e.currentTarget.id);
   }
+  console.log(productList);
 
   return (
     <div>
       <Breadcrumb />
       <SubmenuCategory />
-      {productsSortedList.length ? (
-        <div>
-          <Sort
-            selectedBtn={selectedRadioBtn}
-            onChangeSelectedBtn={onChangeHandler}
-          />
-          <div className="grid gap-4 md:gap-2 grid-cols-6 md:grid-cols-12 max-w-[1700px] mx-auto">
-            {productsSortedList
-              ? productsSortedList.map((product: IProduct) => {
-                  return <Card key={product.slug.current} product={product} />;
-                })
-              : null}
-          </div>
+      {isInNewestProductsPage && productList.length ? (
+        <div className="grid gap-4 md:gap-2 grid-cols-6 md:grid-cols-12 max-w-[1700px] mx-auto">
+          {productList
+            ? productList.map((product: IProduct) => {
+                return <Card key={product.slug.current} product={product} />;
+              })
+            : null}
         </div>
-      ) : productList?.length ? (
+      ) : productList.length ? (
         <div>
           <Sort
             selectedBtn={selectedRadioBtn}
