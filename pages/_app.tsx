24ccc06a29103bfc49ee2a7bf2efaf02d547e294
ZahraMirzaei.JS <z.mirzaei2019@gmail.com>
2022-08-03 17:01:18 +0430

    style: :lipstick: import react-toastify style file to _app.tsx

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -5,6 +5,8 @@ import Layout from "../components/layout/Layout";
 import "slick-carousel/slick/slick.css";
 import "slick-carousel/slick/slick-theme.css";
 
+import "react-toastify/dist/ReactToastify.css";
+
 import "../styles/globals.css";
 
 function MyApp({ Component, pageProps }: AppProps) {
