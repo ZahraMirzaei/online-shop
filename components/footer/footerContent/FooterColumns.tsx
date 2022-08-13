badd1f423d951a875f08b439cd17f7c4277ce028
ZahraMirzaei.JS <z.mirzaei2019@gmail.com>
2022-08-13 12:22:40 +0430

    style: :lipstick: change footer margin spacing

--- components/footer/footerContent/FooterColumns.tsx
+++ components/footer/footerContent/FooterColumns.tsx
@@ -6,10 +6,10 @@ import { useLanguage } from "../../../hooks/useLanguage";
 const FooterColumns = () => {
   const { t } = useLanguage();
   return (
-    <div className="flex flex-wrap flex-grow min-width-[800px] xl:rtl:pl-60 justify-between">
+    <div className="flex justify-between flex-wrap flex-grow min-width-[800px] xl:rtl:pl-60">
       {footerContent.map((item) => {
         return (
-          <div className="mt-4 sm:mt-0" key={item.title}>
+          <div className="mt-6 sm:mt-0" key={item.title}>
             <h4 className="text-md rtl:border-r-4 ltr:border-l-4 border-palette-primary px-2">
               {t[item.title]}
             </h4>
