8d3df00e507296fa8bb1e79498d4d2f24fbec742
ZahraMirzaei.JS <z.mirzaei2019@gmail.com>
2022-08-17 20:46:40 +0430

    feat: :wheelchair: add aria-label attribute to LoginBtn link

--- components/header/user/LoginBtn.tsx
+++ components/header/user/LoginBtn.tsx
@@ -7,7 +7,7 @@ const LoginBtn = () => {
   const { t } = useLanguage();
   return (
     <Link href={"/login"}>
-      <a>
+      <a aria-label="User actions">
         <div className="hidden md:flex items-center rounded-lg  py-1 px-2 ltr:mr-3 rtl:ml-3 border-[1px] border-gray-200 dark:border-gray-200/40 shadow-sm ">
           <HiOutlineLogin style={{ fontSize: "1.6rem" }} />
           <p className="ltr:ml-2 rtl:mr-2 text-xs">
