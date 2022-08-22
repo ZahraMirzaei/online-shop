290da69e8b7e01a159a29651d388bd38254d9676
ZahraMirzaei.JS <z.mirzaei2019@gmail.com>
2022-08-22 20:08:33 +0430

    perf: :zap: dynamic import in index Home page

--- pages/index.tsx
+++ pages/index.tsx
@@ -10,14 +10,10 @@ import { client } from "../lib/client";
 
 import Benefits from "../components/Benefits";
 import Carousel from "../components/carousel";
-import Offers from "../components/Offers/Offers";
-import Category from "../components/category/Category";
-import Newest from "../components/newest/Newest";
-import Brands from "../components/brands";
-
-// import Banners from "../components/banners";
-//used dynamic() to import Banners because I use setInterval for countdown component and
-//I got Error in console (Error:"text content did not match. server client nextjs")
+const Offers = dynamic(() => import("../components/Offers/Offers"));
+const Category = dynamic(() => import("../components/category/Category"));
+const Newest = dynamic(() => import("../components/newest/Newest"));
+const Brands = dynamic(() => import("../components/brands"));
 const Banners = dynamic(() => import("../components/banners"), { ssr: false });
 
 import { IProduct } from "../lib/types/products";
@@ -26,17 +22,15 @@ import { newestProductsFn } from "../utilities/sortByTimeStamp";
 const Home: NextPage<{ products: IProduct[] }> = ({ products }) => {
   const dispatch = useDispatch();
 
-  //add products to offers list
   useEffect(() => {
+    //add products to offers list
     const offersProducts = products.filter((item) => item.discount);
     dispatch(specialOfferProductsActions.addProducts(offersProducts));
-  }, [dispatch, products]);
 
-  //add products to newest list
-  useEffect(() => {
+    //add products to newest list
     const sortedProductsByTimeStamp = newestProductsFn(products);
     dispatch(newestProductsActions.addProducts(sortedProductsByTimeStamp));
-  }, [products, dispatch]);
+  }, [dispatch, products]);
 
   return (
     <div>
